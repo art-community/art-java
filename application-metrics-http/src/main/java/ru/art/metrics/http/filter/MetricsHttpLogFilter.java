ccad7390d96c8ea23b80f32ea26061fa9d89e9f5
antonbashir <antonbashir@mail.ru>
2019-08-12 04:14:54 +0300

    fix metrics log filter

--- application-metrics-http/src/main/java/ru/art/metrics/http/filter/MetricsHttpLogFilter.java
+++ application-metrics-http/src/main/java/ru/art/metrics/http/filter/MetricsHttpLogFilter.java
@@ -20,19 +20,18 @@ import org.zalando.logbook.Logbook;
 import org.zalando.logbook.LogbookCreator.Builder;
 import ru.art.http.logger.ZalangoLogbookLogWriter;
 import static org.zalando.logbook.Conditions.exclude;
-import static org.zalando.logbook.Conditions.requestTo;
 import static ru.art.metrics.constants.MetricsModuleConstants.METRICS_PATH;
 
 public interface MetricsHttpLogFilter {
     static Builder logbookWithoutMetricsLogs(Builder builder) {
         return builder
-                .condition(exclude(requestTo(METRICS_PATH)))
+                .condition(exclude(request -> request.getPath().contains(METRICS_PATH)))
                 .writer(new ZalangoLogbookLogWriter());
     }
 
     static Builder logbookWithoutMetricsLogs() {
         return Logbook.builder()
-                .condition(exclude(requestTo(METRICS_PATH)))
+                .condition(exclude(request -> request.getPath().contains(METRICS_PATH)))
                 .writer(new ZalangoLogbookLogWriter());
     }
 }
