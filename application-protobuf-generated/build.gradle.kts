29f654a32505d09acc9ce4b234e6c58da81e6472
antonbashir <antonbashir@mail.ru>
2019-08-12 04:20:48 +0300

    refactoring

--- application-protobuf-generated/build.gradle.kts
+++ application-protobuf-generated/build.gradle.kts
@@ -6,5 +6,5 @@ dependencies {
     with(art.externalDependencyVersionsConfiguration) {
         provided("io.grpc", "grpc-stub", grpcVersion).exclude("com.google.guava", "guava")
         provided("io.grpc", "grpc-protobuf", grpcVersion).exclude("com.google.guava", "guava")
-    }
+   }
 }
