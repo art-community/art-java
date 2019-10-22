3aa359bc3f0b402b0ee999835b9f1f44b6021312
antonbashir <antonbashir@mail.ru>
2019-10-22 23:53:08 +0300

    imrove factory

--- application-core/src/main/java/ru/art/core/factory/CollectionsFactory.java
+++ application-core/src/main/java/ru/art/core/factory/CollectionsFactory.java
@@ -70,13 +70,81 @@ public class CollectionsFactory {
         return isEmpty(elements) ? emptyList() : asList(elements);
     }
 
+    public static <T> List<T> fixedArrayOf(Stream<T> stream) {
+        return isEmpty(stream) ? emptyList() : asList(cast(stream.toArray()));
+    }
+
+
+    public static List<Long> arrayOf(long[] elements) {
+        if (isEmpty(elements)) {
+            return emptyList();
+        }
+        List<Long> array = arrayOf(elements.length);
+        for (long element : elements) {
+            array.add(element);
+        }
+        return array;
+    }
+
+    public static List<Integer> arrayOf(int[] elements) {
+        if (isEmpty(elements)) {
+            return emptyList();
+        }
+        List<Integer> array = arrayOf(elements.length);
+        for (int element : elements) {
+            array.add(element);
+        }
+        return array;
+    }
+
+    public static List<Byte> arrayOf(byte[] elements) {
+        if (isEmpty(elements)) {
+            return emptyList();
+        }
+        List<Byte> array = arrayOf(elements.length);
+        for (byte element : elements) {
+            array.add(element);
+        }
+        return array;
+    }
+
+    public static List<Double> arrayOf(double[] elements) {
+        if (isEmpty(elements)) {
+            return emptyList();
+        }
+        List<Double> array = arrayOf(elements.length);
+        for (double element : elements) {
+            array.add(element);
+        }
+        return array;
+    }
+
+    public static List<Float> arrayOf(float[] elements) {
+        if (isEmpty(elements)) {
+            return emptyList();
+        }
+        List<Float> array = arrayOf(elements.length);
+        for (float element : elements) {
+            array.add(element);
+        }
+        return array;
+    }
+
+    public static List<Boolean> arrayOf(boolean[] elements) {
+        if (isEmpty(elements)) {
+            return emptyList();
+        }
+        List<Boolean> array = arrayOf(elements.length);
+        for (boolean element : elements) {
+            array.add(element);
+        }
+        return array;
+    }
+
     public static <T> List<T> arrayOf(int size) {
         return size <= 0 ? new ArrayList<>() : new ArrayList<>(size);
     }
 
-    public static <T> List<T> fixedArrayOf(Stream<T> stream) {
-        return isEmpty(stream) ? emptyList() : asList(cast(stream.toArray()));
-    }
 
     @SafeVarargs
     public static <T> List<T> dynamicArrayOf(T... elements) {
@@ -85,6 +153,30 @@ public class CollectionsFactory {
 
     public static <T> List<T> dynamicArrayOf(Collection<T> elements) {
         return isEmpty(elements) ? new ArrayList<>() : new ArrayList<>(elements);
+    }
+
+    public static List<Long> dynamicArrayOf(long[] elements) {
+        return arrayOf(elements);
+    }
+
+    public static List<Integer> dynamicArrayOf(int[] elements) {
+        return arrayOf(elements);
+    }
+
+    public static List<Byte> dynamicArrayOf(byte[] elements) {
+        return arrayOf(elements);
+    }
+
+    public static List<Double> dynamicArrayOf(double[] elements) {
+        return arrayOf(elements);
+    }
+
+    public static List<Float> dynamicArrayOf(float[] elements) {
+        return arrayOf(elements);
+    }
+
+    public static List<Boolean> dynamicArrayOf(boolean[] elements) {
+        return arrayOf(elements);
     }
 
     @SafeVarargs
@@ -96,6 +188,73 @@ public class CollectionsFactory {
         return isEmpty(elements) ? new LinkedList<>() : new LinkedList<>(elements);
     }
 
+    public static List<Long> linkedListOf(long[] elements) {
+        if (isEmpty(elements)) {
+            return linkedListOf();
+        }
+        List<Long> array = linkedListOf();
+        for (long element : elements) {
+            array.add(element);
+        }
+        return array;
+    }
+
+    public static List<Integer> linkedListOf(int[] elements) {
+        if (isEmpty(elements)) {
+            return linkedListOf();
+        }
+        List<Integer> array = linkedListOf();
+        for (int element : elements) {
+            array.add(element);
+        }
+        return array;
+    }
+
+    public static List<Byte> linkedListOf(byte[] elements) {
+        if (isEmpty(elements)) {
+            return linkedListOf();
+        }
+        List<Byte> array = linkedListOf();
+        for (byte element : elements) {
+            array.add(element);
+        }
+        return array;
+    }
+
+    public static List<Double> linkedListOf(double[] elements) {
+        if (isEmpty(elements)) {
+            return linkedListOf();
+        }
+        List<Double> array = linkedListOf();
+        for (double element : elements) {
+            array.add(element);
+        }
+        return array;
+    }
+
+    public static List<Float> linkedListOf(float[] elements) {
+        if (isEmpty(elements)) {
+            return linkedListOf();
+        }
+        List<Float> array = linkedListOf();
+        for (float element : elements) {
+            array.add(element);
+        }
+        return array;
+    }
+
+    public static List<Boolean> linkedListOf(boolean[] elements) {
+        if (isEmpty(elements)) {
+            return linkedListOf();
+        }
+        List<Boolean> array = linkedListOf();
+        for (boolean element : elements) {
+            array.add(element);
+        }
+        return array;
+    }
+
+
     public static <T> Deque<T> dequeOf(Collection<T> elements) {
         return isEmpty(elements) ? new LinkedList<>() : new LinkedList<>(elements);
     }
@@ -105,10 +264,144 @@ public class CollectionsFactory {
         return isEmpty(elements) ? new LinkedList<>() : new LinkedList<>(asList(elements));
     }
 
+    public static Deque<Long> dequeOf(long[] elements) {
+        if (isEmpty(elements)) {
+            return dequeOf();
+        }
+        Deque<Long> deque = dequeOf();
+        for (long element : elements) {
+            deque.add(element);
+        }
+        return deque;
+    }
+
+    public static Deque<Integer> dequeOf(int[] elements) {
+        if (isEmpty(elements)) {
+            return dequeOf();
+        }
+        Deque<Integer> deque = dequeOf();
+        for (int element : elements) {
+            deque.add(element);
+        }
+        return deque;
+    }
+
+    public static Deque<Byte> dequeOf(byte[] elements) {
+        if (isEmpty(elements)) {
+            return dequeOf();
+        }
+        Deque<Byte> deque = dequeOf();
+        for (byte element : elements) {
+            deque.add(element);
+        }
+        return deque;
+    }
+
+    public static Deque<Double> dequeOf(double[] elements) {
+        if (isEmpty(elements)) {
+            return dequeOf();
+        }
+        Deque<Double> deque = dequeOf();
+        for (double element : elements) {
+            deque.add(element);
+        }
+        return deque;
+    }
+
+    public static Deque<Float> dequeOf(float[] elements) {
+        if (isEmpty(elements)) {
+            return dequeOf();
+        }
+        Deque<Float> deque = dequeOf();
+        for (float element : elements) {
+            deque.add(element);
+        }
+        return deque;
+    }
+
+    public static Deque<Boolean> dequeOf(boolean[] elements) {
+        if (isEmpty(elements)) {
+            return dequeOf();
+        }
+        Deque<Boolean> deque = dequeOf();
+        for (boolean element : elements) {
+            deque.add(element);
+        }
+        return deque;
+    }
+
+
     @SafeVarargs
     public static <T> Queue<T> queueOf(T... elements) {
         return isEmpty(elements) ? new LinkedList<>() : new LinkedList<>(asList(elements));
     }
+
+    public static Queue<Long> queueOf(long[] elements) {
+        if (isEmpty(elements)) {
+            return queueOf();
+        }
+        Queue<Long> queue = queueOf();
+        for (long element : elements) {
+            queue.add(element);
+        }
+        return queue;
+    }
+
+    public static Queue<Integer> queueOf(int[] elements) {
+        if (isEmpty(elements)) {
+            return queueOf();
+        }
+        Queue<Integer> queue = queueOf();
+        for (int element : elements) {
+            queue.add(element);
+        }
+        return queue;
+    }
+
+    public static Queue<Byte> queueOf(byte[] elements) {
+        if (isEmpty(elements)) {
+            return queueOf();
+        }
+        Queue<Byte> queue = queueOf();
+        for (byte element : elements) {
+            queue.add(element);
+        }
+        return queue;
+    }
+
+    public static Queue<Double> queueOf(double[] elements) {
+        if (isEmpty(elements)) {
+            return queueOf();
+        }
+        Queue<Double> queue = queueOf();
+        for (double element : elements) {
+            queue.add(element);
+        }
+        return queue;
+    }
+
+    public static Queue<Float> queueOf(float[] elements) {
+        if (isEmpty(elements)) {
+            return queueOf();
+        }
+        Queue<Float> queue = queueOf();
+        for (float element : elements) {
+            queue.add(element);
+        }
+        return queue;
+    }
+
+    public static Queue<Boolean> queueOf(boolean[] elements) {
+        if (isEmpty(elements)) {
+            return queueOf();
+        }
+        Queue<Boolean> deque = queueOf();
+        for (boolean element : elements) {
+            deque.add(element);
+        }
+        return deque;
+    }
+
 
     @SafeVarargs
     public static <T> PriorityQueue<T> priorityQueueOf(Comparator<T> comparator, T... elements) {
@@ -121,6 +414,59 @@ public class CollectionsFactory {
         PriorityQueue<T> queue = new PriorityQueue<>(comparator);
         queue.addAll(elements);
         return isEmpty(elements) ? new PriorityQueue<>(comparator) : queue;
+    }
+
+    public static PriorityQueue<Long> priorityQueueOf(Comparator<Long> comparator, long[] elements) {
+        PriorityQueue<Long> queue = new PriorityQueue<>(comparator);
+        for (long element : elements) {
+            queue.add(element);
+        }
+        return isEmpty(elements) ? new PriorityQueue<>(comparator) : queue;
+    }
+
+    public static PriorityQueue<Integer> priorityQueueOf(Comparator<Integer> comparator, int[] elements) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(comparator);
+        for (int element : elements) {
+            queue.add(element);
+        }
+        return isEmpty(elements) ? new PriorityQueue<>(comparator) : queue;
+
+    }
+
+    public static PriorityQueue<Byte> priorityQueueOf(Comparator<Byte> comparator, byte[] elements) {
+        PriorityQueue<Byte> queue = new PriorityQueue<>(comparator);
+        for (byte element : elements) {
+            queue.add(element);
+        }
+        return isEmpty(elements) ? new PriorityQueue<>(comparator) : queue;
+
+    }
+
+    public static PriorityQueue<Double> priorityQueueOf(Comparator<Double> comparator, double[] elements) {
+        PriorityQueue<Double> queue = new PriorityQueue<>(comparator);
+        for (double element : elements) {
+            queue.add(element);
+        }
+        return isEmpty(elements) ? new PriorityQueue<>(comparator) : queue;
+
+    }
+
+    public static PriorityQueue<Float> priorityQueueOf(Comparator<Float> comparator, float[] elements) {
+        PriorityQueue<Float> queue = new PriorityQueue<>(comparator);
+        for (float element : elements) {
+            queue.add(element);
+        }
+        return isEmpty(elements) ? new PriorityQueue<>(comparator) : queue;
+
+    }
+
+    public static PriorityQueue<Boolean> priorityQueueOf(Comparator<Boolean> comparator, boolean[] elements) {
+        PriorityQueue<Boolean> queue = new PriorityQueue<>(comparator);
+        for (boolean element : elements) {
+            queue.add(element);
+        }
+        return isEmpty(elements) ? new PriorityQueue<>(comparator) : queue;
+
     }
 
     @SafeVarargs
@@ -138,6 +484,72 @@ public class CollectionsFactory {
 
     public static <T> Set<T> setOf(Collection<T> elements) {
         return isEmpty(elements) ? new LinkedHashSet<>() : new LinkedHashSet<>(elements);
+    }
+
+    public static Set<Long> setOf(long[] elements) {
+        if (isEmpty(elements)) {
+            return setOf();
+        }
+        Set<Long> set = setOf();
+        for (long element : elements) {
+            set.add(element);
+        }
+        return set;
+    }
+
+    public static Set<Integer> setOf(int[] elements) {
+        if (isEmpty(elements)) {
+            return setOf();
+        }
+        Set<Integer> set = setOf();
+        for (int element : elements) {
+            set.add(element);
+        }
+        return set;
+    }
+
+    public static Set<Byte> setOf(byte[] elements) {
+        if (isEmpty(elements)) {
+            return setOf();
+        }
+        Set<Byte> set = setOf();
+        for (byte element : elements) {
+            set.add(element);
+        }
+        return set;
+    }
+
+    public static Set<Double> setOf(double[] elements) {
+        if (isEmpty(elements)) {
+            return setOf();
+        }
+        Set<Double> set = setOf();
+        for (double element : elements) {
+            set.add(element);
+        }
+        return set;
+    }
+
+    public static Set<Float> setOf(float[] elements) {
+        if (isEmpty(elements)) {
+            return setOf();
+        }
+        Set<Float> set = setOf();
+        for (float element : elements) {
+            set.add(element);
+        }
+        return set;
+    }
+
+    public static Set<Boolean> setOf(boolean[] elements) {
+        if (isEmpty(elements)) {
+            return setOf();
+        }
+        Set<Boolean> set = setOf();
+        for (boolean element : elements) {
+            set.add(element);
+        }
+        return set;
     }
 
     @SafeVarargs
@@ -162,6 +574,61 @@ public class CollectionsFactory {
         treeSet.addAll(elements);
         return treeSet;
     }
+
+    public static Set<Long> treeOf(Comparator<Long> comparator, long[] elements) {
+        TreeSet<Long> treeSet = new TreeSet<>();
+        if (isEmpty(elements)) return treeSet;
+        for (long element : elements) {
+            treeSet.add(element);
+        }
+        return treeSet;
+    }
+
+    public static Set<Integer> treeOf(Comparator<Integer> comparator, int[] elements) {
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        if (isEmpty(elements)) return treeSet;
+        for (int element : elements) {
+            treeSet.add(element);
+        }
+        return treeSet;
+    }
+
+    public static Set<Byte> treeOf(Comparator<Byte> comparator, byte[] elements) {
+        TreeSet<Byte> treeSet = new TreeSet<>();
+        if (isEmpty(elements)) return treeSet;
+        for (byte element : elements) {
+            treeSet.add(element);
+        }
+        return treeSet;
+  }
+
+    public static Set<Double> treeOf(Comparator<Double> comparator, double[] elements) {
+        TreeSet<Double> treeSet = new TreeSet<>();
+        if (isEmpty(elements)) return treeSet;
+        for (double element : elements) {
+            treeSet.add(element);
+        }
+        return treeSet;
+  }
+
+    public static Set<Float> treeOf(Comparator<Float> comparator, float[] elements) {
+        TreeSet<Float> treeSet = new TreeSet<>();
+        if (isEmpty(elements)) return treeSet;
+        for (float element : elements) {
+            treeSet.add(element);
+        }
+        return treeSet;
+    }
+
+    public static Set<Boolean> treeOf(Comparator<Boolean> comparator, boolean[] elements) {
+        TreeSet<Boolean> treeSet = new TreeSet<>();
+        if (isEmpty(elements)) return treeSet;
+        for (boolean element : elements) {
+            treeSet.add(element);
+        }
+        return treeSet;
+    }
+
 
     public static <K, V> Pair<K, V> pairOf(K k, V v) {
         return new Pair<>(k, v);
