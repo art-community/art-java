87b597ad536b2b5221221cd2d6d5fc84578faa82
antonbashir <antonbashir@mail.ru>
2019-10-28 03:51:14 +0300

    fix npe

--- application-logging/build.gradle.kts
+++ application-logging/build.gradle.kts
@@ -27,9 +27,10 @@ dependencies {
     with(art.externalDependencyVersionsConfiguration) {
         embedded("org.slf4j", "jul-to-slf4j", sl4jVersion)
 
-        embedded("org.apache.logging.log4j", "log4j-api", log4jVersion)
-        embedded("org.apache.logging.log4j", "log4j-core", log4jVersion)
-        embedded("org.apache.logging.log4j", "log4j-slf4j-impl", log4jVersion)
+        embedded("org.apache.logging.log4j", "log4j-api", "2.12.1")
+        embedded("org.apache.logging.log4j", "log4j-core", "2.12.1")
+        embedded("org.apache.logging.log4j", "log4j-web", "2.12.1")
+        embedded("org.apache.logging.log4j", "log4j-slf4j-impl", "2.12.1")
 
         embedded("com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml", jacksonVersion)
         embedded("com.fasterxml.jackson.core", "jackson-core", jacksonVersion)
--- management-panel/build.gradle.kts
+++ management-panel/build.gradle.kts
@@ -1,3 +1,21 @@
+/*
+ * ART Java
+ *
+ * Copyright 2019 ART
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.gradle.api.JavaVersion.*
 import org.jetbrains.kotlin.gradle.tasks.*
 import org.gradle.internal.os.OperatingSystem as os
@@ -25,11 +43,11 @@ dependencies {
 
 
 tasks.withType<KotlinCompile> {
-    sourceCompatibility = VERSION_1_8.toString()
-    targetCompatibility = VERSION_1_8.toString()
+    sourceCompatibility = VERSION_11.toString()
+    targetCompatibility = VERSION_11.toString()
 
     kotlinOptions {
-        jvmTarget = VERSION_1_8.toString()
+        jvmTarget = VERSION_11.toString()
     }
 }
 
