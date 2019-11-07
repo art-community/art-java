9930bc1b91314fac2a70166d37c99802d9a74b37
Daniil Titov <Daniil.A.Titov@gmail.com>
2019-11-07 17:25:33 +0300

    refactoring

--- application-information/src/main/java/ru/art/information/mapping/RsocketServiceInformationMapper.java
+++ application-information/src/main/java/ru/art/information/mapping/RsocketServiceInformationMapper.java
@@ -18,27 +18,26 @@
 
 package ru.art.information.mapping;
 
-import static ru.art.core.checker.CheckerForEmptiness.isNotEmpty;
-
-import java.lang.String;
 import ru.art.entity.Entity;
 import ru.art.entity.PrimitiveMapping;
 import ru.art.entity.mapper.ValueFromModelMapper;
 import ru.art.entity.mapper.ValueToModelMapper;
 import ru.art.information.model.RsocketServiceInformation;
 
+import static ru.art.core.checker.CheckerForEmptiness.isNotEmpty;
+
 public interface RsocketServiceInformationMapper {
-	String id = "id";
+    String id = "id";
 
-	String methods = "methods";
+    String methods = "methods";
 
-	ValueToModelMapper<RsocketServiceInformation, Entity> toRsocketServiceInformation = entity -> isNotEmpty(entity) ? RsocketServiceInformation.builder()
-			.id(entity.getString(id))
-			.methods(entity.getMap(methods, PrimitiveMapping.StringPrimitive.toModel, RsocketServiceMethodInformationMapper.toRsocketServiceMethodInformation))
-			.build() : RsocketServiceInformation.builder().build();
+    ValueToModelMapper<RsocketServiceInformation, Entity> toRsocketServiceInformation = entity -> isNotEmpty(entity) ? RsocketServiceInformation.builder()
+            .id(entity.getString(id))
+            .methods(entity.getMap(methods, PrimitiveMapping.StringPrimitive.toModel, RsocketServiceMethodInformationMapper.toRsocketServiceMethodInformation))
+            .build() : RsocketServiceInformation.builder().build();
 
-	ValueFromModelMapper<RsocketServiceInformation, Entity> fromRsocketServiceInformation = model -> isNotEmpty(model) ? Entity.entityBuilder()
-			.stringField(id, model.getId())
-			.mapField(methods, model.getMethods(), PrimitiveMapping.StringPrimitive.fromModel, RsocketServiceMethodInformationMapper.fromRsocketServiceMethodInformation)
-			.build() : Entity.entityBuilder().build();
+    ValueFromModelMapper<RsocketServiceInformation, Entity> fromRsocketServiceInformation = model -> isNotEmpty(model) ? Entity.entityBuilder()
+            .stringField(id, model.getId())
+            .mapField(methods, model.getMethods(), PrimitiveMapping.StringPrimitive.fromModel, RsocketServiceMethodInformationMapper.fromRsocketServiceMethodInformation)
+            .build() : Entity.entityBuilder().build();
 }
--- application-information/src/main/java/ru/art/information/mapping/RsocketServiceMethodInformationMapper.java
+++ application-information/src/main/java/ru/art/information/mapping/RsocketServiceMethodInformationMapper.java
@@ -18,30 +18,29 @@
 
 package ru.art.information.mapping;
 
-import static ru.art.core.checker.CheckerForEmptiness.isNotEmpty;
-
-import java.lang.String;
 import ru.art.entity.Entity;
 import ru.art.entity.mapper.ValueFromModelMapper;
 import ru.art.entity.mapper.ValueToModelMapper;
 import ru.art.information.model.RsocketServiceMethodInformation;
 
+import static ru.art.core.checker.CheckerForEmptiness.isNotEmpty;
+
 public interface RsocketServiceMethodInformationMapper {
-	String id = "id";
+    String id = "id";
 
-	String exampleRequest = "exampleRequest";
+    String exampleRequest = "exampleRequest";
 
-	String exampleResponse = "exampleResponse";
+    String exampleResponse = "exampleResponse";
 
-	ValueToModelMapper<RsocketServiceMethodInformation, Entity> toRsocketServiceMethodInformation = entity -> isNotEmpty(entity) ? RsocketServiceMethodInformation.builder()
-			.id(entity.getString(id))
-			.exampleRequest(entity.getString(exampleRequest))
-			.exampleResponse(entity.getString(exampleResponse))
-			.build() : RsocketServiceMethodInformation.builder().build();
+    ValueToModelMapper<RsocketServiceMethodInformation, Entity> toRsocketServiceMethodInformation = entity -> isNotEmpty(entity) ? RsocketServiceMethodInformation.builder()
+            .id(entity.getString(id))
+            .exampleRequest(entity.getString(exampleRequest))
+            .exampleResponse(entity.getString(exampleResponse))
+            .build() : RsocketServiceMethodInformation.builder().build();
 
-	ValueFromModelMapper<RsocketServiceMethodInformation, Entity> fromRsocketServiceMethodInformation = model -> isNotEmpty(model) ? Entity.entityBuilder()
-			.stringField(id, model.getId())
-			.stringField(exampleRequest, model.getExampleRequest())
-			.stringField(exampleResponse, model.getExampleResponse())
-			.build() : Entity.entityBuilder().build();
+    ValueFromModelMapper<RsocketServiceMethodInformation, Entity> fromRsocketServiceMethodInformation = model -> isNotEmpty(model) ? Entity.entityBuilder()
+            .stringField(id, model.getId())
+            .stringField(exampleRequest, model.getExampleRequest())
+            .stringField(exampleResponse, model.getExampleResponse())
+            .build() : Entity.entityBuilder().build();
 }
