5bf76bac28a18338728d4cd8755d86fffa2f4bd1
antonbashir <antonbashir@mail.ru>
2019-11-11 10:26:22 +0300

    cmake changes

--- application-reindexer/src/main/cpp/src/Main.cpp
+++ application-reindexer/src/main/cpp/src/Main.cpp
@@ -1,27 +1,30 @@
-#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <string>
 #include "Main.h"
-#include "../include/reindexer_c.h"
+#include "../reindexer/cpp_src/core/reindexer.h"
 
 void initializeReindexer();
 
 JNIEXPORT void JNICALL
-Java_Main_initializeReindexer(JNIEnv *env, jobject obj)
+Java_Main_initializeReindexer(JNIEnv* env, jobject obj)
 {
     initializeReindexer();
 }
 
 void initializeReindexer()
 {
+    auto reindexer =
     uintptr_t reindexer = init_reindexer();
-    const char *string = R"(C:\\Development\\Projects\\ART\\application-reindexer\\src\\main\\c\\reindexer)";
+    const char* string = R"(C:\\Development\\Projects\\ART\\application-reindexer\\src\\main\\c\\reindexer)";
     reindexer_string reindexerString;
-    reindexerString.p = (void *) string;
+    reindexerString.p = (void*) string;
     reindexerString.n = _mbstrlen(string);
     const reindexer_error &error = reindexer_connect(reindexer, reindexerString, ConnectOpts());
     printf("Result of initializing: %d with what: \"%s\"\n", error.code, error.what);
 }
 
-int main(int argumentCount, char **arguments)
+int main(int argumentCount, char** arguments)
 {
     initializeReindexer();
     return 0;
