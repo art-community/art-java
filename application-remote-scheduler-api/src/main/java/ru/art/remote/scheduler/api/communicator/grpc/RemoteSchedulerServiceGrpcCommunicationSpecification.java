796e686452a5e411e75f5bacbdf7e4d256b97663
Padmelina <Padmelina93@gmail.com>
2019-12-27 18:00:28 +0300

    add correct empty grpc communicator

--- application-remote-scheduler-api/src/main/java/ru/art/remote/scheduler/api/communicator/grpc/RemoteSchedulerServiceGrpcCommunicationSpecification.java
+++ application-remote-scheduler-api/src/main/java/ru/art/remote/scheduler/api/communicator/grpc/RemoteSchedulerServiceGrpcCommunicationSpecification.java
@@ -3,13 +3,11 @@ package ru.art.remote.scheduler.api.communicator.grpc;
 import lombok.Getter;
 import ru.art.grpc.client.specification.GrpcCommunicationSpecification;
 
+import static ru.art.remote.scheduler.api.constants.RemoteSchedulerApiConstants.REMOTE_SCHEDULER_SERVICE_ID;
+
 @Getter
 public class RemoteSchedulerServiceGrpcCommunicationSpecification implements GrpcCommunicationSpecification {
-    @Override
-    public String getServiceId() {
-        return null;
-    }
-
+    private final String serviceId = REMOTE_SCHEDULER_SERVICE_ID;
     @Override
     public <P, R> R executeMethod(String methodId, P request) {
         return null;
