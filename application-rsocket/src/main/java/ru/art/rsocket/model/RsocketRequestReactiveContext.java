5164ffa8fadc752d7376befb3cd46d63ff52c19a
antonbashir <antonbashir@mail.ru>
2020-01-13 04:24:06 +0300

    fix build

--- application-rsocket/src/main/java/ru/art/rsocket/model/RsocketRequestReactiveContext.java
+++ application-rsocket/src/main/java/ru/art/rsocket/model/RsocketRequestReactiveContext.java
@@ -56,6 +56,7 @@ public class RsocketRequestReactiveContext {
         Value payloadValue;
         try {
             payloadValue = readPayloadData(payload, dataFormat);
+            payload.release(payload.refCnt());
         } catch (Throwable throwable) {
             if (rsocketModule().isEnableRawDataTracing()) {
                 loggingModule().getLogger(RsocketRequestContext.class).error(format(FAILED_TO_READ_PAYLOAD, throwable.getMessage(), throwable));
