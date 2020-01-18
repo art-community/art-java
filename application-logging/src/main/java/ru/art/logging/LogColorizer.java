1260787a52226294a4516c2d35344e1d64247583
antonbashir <antonbashir@mail.ru>
2020-01-18 05:31:21 +0300

    add tarantool startup timeout

--- application-logging/src/main/java/ru/art/logging/LogColorizer.java
+++ application-logging/src/main/java/ru/art/logging/LogColorizer.java
@@ -2,31 +2,11 @@ package ru.art.logging;
 
 import lombok.experimental.*;
 import org.apache.logging.log4j.*;
-import static ru.art.core.constants.StringConstants.*;
-import static ru.art.logging.LogMessageColor.*;
+import static ru.art.core.colorizer.AnsiColorizer.*;
+import static ru.art.core.constants.AnsiColor.*;
 
 @UtilityClass
 public class LogColorizer {
-    public static String message(String message, LogMessageColor color) {
-        return color.getCode() + message + ANSI_RESET;
-    }
-
-    public static String success(String message) {
-        return message(message, GREEN_BOLD);
-    }
-
-    public static String error(String message) {
-        return message(message, RED);
-    }
-
-    public static String warning(String message) {
-        return message(message, YELLOW);
-    }
-
-    public static String additional(String message) {
-        return message(message, PURPLE_BOLD);
-    }
-
     public static String byLevel(Level level, String message) {
         switch (level.getStandardLevel()) {
             case FATAL:
