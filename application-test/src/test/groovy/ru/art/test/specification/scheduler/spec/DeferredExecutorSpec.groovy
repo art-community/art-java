cebd34bad7d89c1473b94a2c261328afce2607fc
antonbashir <antonbashir@mail.ru>
2020-01-26 04:52:35 +0300

    refactoring

--- application-test/src/test/groovy/ru/art/test/specification/scheduler/spec/DeferredExecutorSpec.groovy
+++ application-test/src/test/groovy/ru/art/test/specification/scheduler/spec/DeferredExecutorSpec.groovy
@@ -218,17 +218,21 @@ class DeferredExecutorSpec extends Specification {
 
         when:
         println "Executor isWorking with $infinityCount infinity events and $longCount long events"
-        def infinityEvents = infinityEventsTriggerTimes.collect {
-            executor.submit createInfinityDeferredEventTask(it, {
-                addEventResult actualResults;
-                actualResults.size() - 1
-            }), it
+        runInJoinedThread {
+            infinityEventsTriggerTimes.collect {
+                executor.submit createInfinityDeferredEventTask(it, {
+                    addEventResult actualResults;
+                    actualResults.size() - 1
+                }), it
+            }
         }
-        longEventsTriggerTimes.collect {
-            executor.submit createLongDeferredEventTask(it, {
-                addEventResult actualResults;
-                actualResults.size() - 1
-            }), it
+        runInJoinedThread {
+            longEventsTriggerTimes.collect {
+                executor.submit createLongDeferredEventTask(it, {
+                    addEventResult actualResults;
+                    actualResults.size() - 1
+                }), it
+            }
         }
         sleep 5000
 
@@ -259,7 +263,9 @@ class DeferredExecutorSpec extends Specification {
 
         when:
         println "Executor isWorking with $eventCount events"
-        triggerTimes.collect { addEventToExecutorWithReturningValue executor, it, actualResults, expectedValues.poll() }*.get()
+        runInJoinedThread {
+            triggerTimes.collect { addEventToExecutorWithReturningValue executor, it, actualResults, expectedValues.poll() }*.get()
+        }
 
         then:
         actualResults != null
