d4d85d6263afb5ce304845f73ba62bce7ecc71c7
anton.bashirov <antonbashir@mail.ru>
2020-09-27 03:19:22 +0300

    ART

---
+++ communicator/src/main/java/io/art/communicator/module/CommunicatorModule.java
@@ -0,0 +1,46 @@
+/*
+ * ART
+ *
+ * Copyright 2020 ART
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.art.communicator.module;
+
+import io.art.communicator.configuration.*;
+import io.art.communicator.state.*;
+import io.art.core.module.*;
+import lombok.*;
+import static io.art.communicator.configuration.CommunicatorModuleConfiguration.*;
+import static io.art.core.context.Context.*;
+
+@Getter
+public class CommunicatorModule implements StatefulModule<CommunicatorModuleConfiguration, Configurator, CommunicatorModuleState> {
+    private final String id = CommunicatorModule.class.getSimpleName();
+    private final CommunicatorModuleConfiguration configuration = new CommunicatorModuleConfiguration();
+    private final Configurator configurator = new Configurator(configuration);
+    private final CommunicatorModuleState state = new CommunicatorModuleState();
+    @Getter(lazy = true)
+    private static final
+    StatefulModuleProxy<CommunicatorModuleConfiguration, CommunicatorModuleState> communicatorModule = context().getStatefulModule(CommunicatorModule.class.getSimpleName());
+
+    public static StatefulModuleProxy<CommunicatorModuleConfiguration, CommunicatorModuleState> communicatorModule() {
+        return getCommunicatorModule();
+    }
+
+    public static void main(String[] args) {
+        context().loadModule(new CommunicatorModule());
+        System.out.println(communicatorModule().configuration());
+    }
+}
