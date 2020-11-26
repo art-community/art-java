56d560272d6db760016cd8bd65c32eff239b7099
antomy-gc <antomy-gc@gmail.com>
2020-11-26 11:37:36 +0300

    config provider in progress

--- tarantool/src/main/java/io/art/refactored/configuration/TarantoolModuleConfiguration.java
+++ tarantool/src/main/java/io/art/refactored/configuration/TarantoolModuleConfiguration.java
@@ -1,10 +1,30 @@
 package io.art.refactored.configuration;
 
+import io.art.core.module.ModuleConfiguration;
+import io.art.core.module.ModuleConfigurator;
+import io.art.core.source.ConfigurationSource;
+import lombok.RequiredArgsConstructor;
+
+import static io.art.refactored.constants.TarantoolModuleConstants.ConfigurationKeys.*;
+import static java.util.Optional.*;
+
 import java.util.Map;
 
 import static io.art.core.factory.CollectionsFactory.mapOf;
 
-public class TarantoolModuleConfiguration {
+public class TarantoolModuleConfiguration implements ModuleConfiguration {
     public Map<String, TarantoolInstanceConfiguration> instances = mapOf();
 
+    @RequiredArgsConstructor
+    public static class Configurator implements ModuleConfigurator<TarantoolModuleConfiguration, Configurator>{
+        private final TarantoolModuleConfiguration configuration;
+
+
+    }
 }
+
+/*
+tarantool_refactored:
+    instances:
+
+ */
