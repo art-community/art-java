ee1e4c4431e53d758818e35188b0bc68638619ba
antomy-gc <antomy-gc@gmail.com>
2020-12-31 01:54:36 +0300

    threadLocal transactionManager

--- tarantool/src/main/java/io/art/tarantool/transaction/TarantoolTransactionManager.java
+++ tarantool/src/main/java/io/art/tarantool/transaction/TarantoolTransactionManager.java
@@ -1,5 +1,6 @@
 package io.art.tarantool.transaction;
 
+import io.art.core.threadlocal.ThreadLocalValue;
 import io.art.tarantool.transaction.operation.result.TarantoolOperationResult;
 import io.art.tarantool.transaction.operation.result.TarantoolSingleOperationResult;
 import io.art.tarantool.transaction.operation.result.TarantoolTransactionOperationResult;
@@ -11,27 +12,25 @@ import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 import static io.art.core.factory.ListFactory.linkedList;
+import static io.art.core.threadlocal.ThreadLocalValue.threadLocal;
 import static io.art.tarantool.constants.TarantoolModuleConstants.Functions.TRANSACTION;
 import static io.art.tarantool.transaction.operation.TarantoolTransactionOperation.tarantoolTransactionOperation;
 
 public class TarantoolTransactionManager {
     private final TarantoolClusterClient client;
-    private boolean activeTransaction = false;
-    private boolean isRWTransaction = false;
-    private final List<List<?>> operations = linkedList();
-    private final List<TarantoolTransactionOperationResult<?>> results = linkedList();
+    private final ThreadLocalValue<State> state = threadLocal(State::new);
 
     public TarantoolTransactionManager(TarantoolClusterClient client){
         this.client = client;
     }
 
     public void begin(){
-        activeTransaction = true;
+        state().activeTransaction = true;
     }
 
     public void commit(){
-        CompletableFuture<List<?>> response = call(TRANSACTION, operations);
-        for (TarantoolTransactionOperationResult<?> result : results){
+        CompletableFuture<List<?>> response = call(TRANSACTION, state().operations);
+        for (TarantoolTransactionOperationResult<?> result : state().results){
             result.transactionCommitted(response);
         }
         clearTransaction();
@@ -42,37 +41,48 @@ public class TarantoolTransactionManager {
     }
 
     public TarantoolOperationResult<?> callRW(String function, Function<List<?>, Optional<?>> responseMapper, Object ... args){
-        isRWTransaction = true;
+        state().isRWTransaction = true;
         return callRO(function, responseMapper, args);
     }
 
     public TarantoolOperationResult<?> callRO(String function, Function<List<?>, Optional<?>> responseMapper, Object ... args){
-        return activeTransaction ? addOperation(function, responseMapper, args) : callSingleOperation(function, responseMapper, args);
+        return state().activeTransaction ? addOperation(function, responseMapper, args) : callSingleOperation(function, responseMapper, args);
     }
 
 
 
+    private State state(){
+        return state.get();
+    }
+
     private TarantoolOperationResult<?> addOperation(String function, Function<List<?>, Optional<?>> responseMapper, Object ... args){
-        TarantoolTransactionOperationResult<?> result = new TarantoolTransactionOperationResult<>(operations.size(), responseMapper);
-        operations.add(tarantoolTransactionOperation(function, args));
-        results.add(result);
+        TarantoolTransactionOperationResult<?> result = new TarantoolTransactionOperationResult<>(state().operations.size(), responseMapper);
+        state().operations.add(tarantoolTransactionOperation(function, args));
+        state().results.add(result);
         return result;
     }
 
     private TarantoolOperationResult<?> callSingleOperation(String function, Function<List<?>, Optional<?>> responseMapper, Object ... args){
         TarantoolSingleOperationResult<?> result = new TarantoolSingleOperationResult<>(call(function, args), responseMapper);
-        isRWTransaction = false;
+        state().isRWTransaction = false;
         return result;
     }
 
     private CompletableFuture<List<?>> call(String function, Object... args){
-        return isRWTransaction ? client.callRW(function, args) : client.callRO(function, args);
+        return state().isRWTransaction ? client.callRW(function, args) : client.callRO(function, args);
     }
 
     private void clearTransaction(){
-        operations.clear();
-        results.clear();
-        isRWTransaction = false;
-        activeTransaction = false;
+        state().operations.clear();
+        state().results.clear();
+        state().isRWTransaction = false;
+        state().activeTransaction = false;
+    }
+
+    private static class State {
+        public boolean activeTransaction = false;
+        public boolean isRWTransaction = false;
+        public final List<List<?>> operations = linkedList();
+        public final List<TarantoolTransactionOperationResult<?>> results = linkedList();
     }
 }
