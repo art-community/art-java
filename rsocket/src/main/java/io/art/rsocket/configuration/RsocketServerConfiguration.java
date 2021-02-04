d028b9a869dc3fc1c71bd2ed1543659f8127a8cd
antonbashir <antonbashir@mail.ru>
2021-02-04 08:12:44 +0300

    ART

--- rsocket/src/main/java/io/art/rsocket/configuration/RsocketServerConfiguration.java
+++ rsocket/src/main/java/io/art/rsocket/configuration/RsocketServerConfiguration.java
@@ -35,10 +35,12 @@ import static io.art.core.constants.NetworkConstants.*;
 import static io.art.core.model.ServiceMethodIdentifier.*;
 import static io.art.rsocket.constants.RsocketModuleConstants.ConfigurationKeys.*;
 import static io.art.rsocket.constants.RsocketModuleConstants.Defaults.*;
+import static io.art.rsocket.constants.RsocketModuleConstants.PayloadDecoderMode.DEFAULT;
 import static io.art.rsocket.constants.RsocketModuleConstants.PayloadDecoderMode.*;
 import static io.art.rsocket.constants.RsocketModuleConstants.TransportMode.*;
 import static io.art.value.constants.ValueModuleConstants.DataFormat.*;
 import static io.rsocket.frame.FrameLengthCodec.*;
+import static io.rsocket.frame.decoder.PayloadDecoder.ZERO_COPY;
 
 @Getter
 public class RsocketServerConfiguration {
@@ -78,12 +80,10 @@ public class RsocketServerConfiguration {
         configuration.defaultMetaDataFormat = listener.register(dataFormat(source.getString(DEFAULT_META_DATA_FORMAT_KEY), JSON));
         configuration.logging = listener.register(orElse(source.getBool(LOGGING_KEY), false));
         configuration.fragmentationMtu = listener.register(orElse(source.getInt(FRAGMENTATION_MTU_KEY), 0));
-        configuration.resume = listener.register(source.getNested(RESUME_SECTION, RsocketResumeConfigurator::from));
-        configuration.payloadDecoder = listener.register(rsocketPayloadDecoder(source.getString(PAYLOAD_DECODER_KEY)) == DEFAULT
-                ? PayloadDecoder.DEFAULT
-                : PayloadDecoder.ZERO_COPY);
+        configuration.payloadDecoder = listener.register(rsocketPayloadDecoder(source.getString(PAYLOAD_DECODER_KEY)) == DEFAULT ? PayloadDecoder.DEFAULT : ZERO_COPY);
         configuration.maxInboundPayloadSize = listener.register(orElse(source.getInt(MAX_INBOUND_PAYLOAD_SIZE_KEY), FRAME_LENGTH_MASK));
         configuration.transport = listener.register(rsocketTransport(source.getString(TRANSPORT_MODE_KEY)));
+        configuration.resume = source.getNested(RESUME_SECTION, RsocketResumeConfigurator::from);
 
         String serviceId = source.getString(DEFAULT_SERVICE_ID_KEY);
         String methodId = source.getString(DEFAULT_METHOD_ID_KEY);
@@ -97,11 +97,11 @@ public class RsocketServerConfiguration {
 
         switch (configuration.transport) {
             case TCP:
-                configuration.tcpServer = listener.register(TcpServer.create().port(port).host(host));
+                configuration.tcpServer = TcpServer.create().port(listener.register(port)).host(listener.register(host));
                 configuration.tcpMaxFrameLength = listener.register(orElse(source.getInt(TRANSPORT_TCP_MAX_FRAME_LENGTH), FRAME_LENGTH_MASK));
                 break;
             case WS:
-                configuration.httpWebSocketServer = listener.register(HttpServer.create().port(port).host(host));
+                configuration.httpWebSocketServer = HttpServer.create().port(listener.register(port)).host(listener.register(host));
                 break;
         }
 
