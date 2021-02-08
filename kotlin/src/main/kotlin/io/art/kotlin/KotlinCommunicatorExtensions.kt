23c0c067739b07c3ee9cc617e0d1baf6e1c6087e
antonbashir <antonbashir@mail.ru>
2021-02-08 07:30:22 +0300

    ART

--- kotlin/src/main/kotlin/io/art/kotlin/KotlinCommunicatorExtensions.kt
+++ kotlin/src/main/kotlin/io/art/kotlin/KotlinCommunicatorExtensions.kt
@@ -1,5 +1,15 @@
 package io.art.kotlin
 
 import io.art.communicator.module.CommunicatorModule.communicator
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty
 
-inline fun <reified T> communicator() = communicator(T::class.java)!!
+
+class CommunicatorDelegate {
+    inline operator fun <reified T> getValue(thisRef: Any?, property: KProperty<*>): T = communicator(T::class.java)
+}
+
+fun communicator() = CommunicatorDelegate()
+inline fun <reified T> communicator(): T = communicator(T::class.java)
+fun <T : Any> communicator(proxy: KClass<T>): T = communicator(proxy.java) as T
+fun <T : Any, R : Any> communicator(proxy: KClass<T>, action: T.() -> R): R = action(communicator(proxy.java) as T)
