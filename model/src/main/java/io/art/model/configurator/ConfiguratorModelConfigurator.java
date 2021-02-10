6b2b42bd1d29d344fcbbe5881c6719deec9c689c
antonbashir <antonbashir@mail.ru>
2021-02-10 05:28:22 +0300

    ART

--- model/src/main/java/io/art/model/configurator/ConfiguratorModelConfigurator.java
+++ model/src/main/java/io/art/model/configurator/ConfiguratorModelConfigurator.java
@@ -3,14 +3,15 @@ package io.art.model.configurator;
 import io.art.configurator.model.*;
 import io.art.model.implementation.configurator.*;
 import static io.art.core.constants.StringConstants.*;
+import static io.art.core.factory.ArrayFactory.*;
 import static io.art.core.factory.SetFactory.*;
 import java.util.*;
 
 public class ConfiguratorModelConfigurator {
     private final Set<CustomConfigurationModel> customConfigurations = set();
 
-    public ConfiguratorModelConfigurator configuration(Class<?> model) {
-        customConfigurations.add(new CustomConfigurationModel(EMPTY_STRING, model));
+    public ConfiguratorModelConfigurator configuration(Class<?>... models) {
+        streamOf(models).forEach(model -> customConfigurations.add(new CustomConfigurationModel(EMPTY_STRING, model)));
         return this;
     }
 
--- model/src/main/java/io/art/model/configurator/ValueModelConfigurator.java
+++ model/src/main/java/io/art/model/configurator/ValueModelConfigurator.java
@@ -1,6 +1,7 @@
 package io.art.model.configurator;
 
 import io.art.model.implementation.value.*;
+import static io.art.core.factory.ArrayFactory.*;
 import static io.art.core.factory.SetFactory.*;
 import java.lang.reflect.*;
 import java.util.*;
@@ -8,8 +9,8 @@ import java.util.*;
 public class ValueModelConfigurator {
     private final Set<Type> mappedTypes = set();
 
-    public  ValueModelConfigurator mapping(Type type) {
-        mappedTypes.add(type);
+    public ValueModelConfigurator mapping(Type... types) {
+        streamOf(types).forEach(mappedTypes::add);
         return this;
     }
 
