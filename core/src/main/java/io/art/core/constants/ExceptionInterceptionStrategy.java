3fec846f3444dfdada885d51adf07d982b44ccdf
anton.bashirov <anton.sh.local@gmail.com>
2021-04-17 00:03:19 +0300

    ART

--- core/src/main/java/io/art/core/constants/ExceptionInterceptionStrategy.java
+++ core/src/main/java/io/art/core/constants/ExceptionInterceptionStrategy.java
@@ -2,7 +2,7 @@
 /*
  * ART
  *
- * Copyright 2020 ART
+ * Copyright 2019-2021 ART
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
