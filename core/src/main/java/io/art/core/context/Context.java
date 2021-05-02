5505b8e62d7b7ff9e777cbe6421ba38af762c958
anton.bashirov <anton.sh.local@gmail.com>
2021-05-02 17:32:59 +0300

    ART

--- core/src/main/java/io/art/core/context/Context.java
+++ core/src/main/java/io/art/core/context/Context.java
@@ -23,6 +23,7 @@ import io.art.core.collection.*;
 import io.art.core.configuration.*;
 import io.art.core.exception.*;
 import io.art.core.module.*;
+import io.art.core.module.Module;
 import io.art.core.property.*;
 import static io.art.core.caster.Caster.*;
 import static io.art.core.changes.ChangesListener.*;
