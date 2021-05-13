b480e6c3e9688029745fcf15c237954bf2733d60
anton.bashirov <anton.sh.local@gmail.com>
2021-05-13 05:47:34 +0300

    ART

--- logging/src/main/java/io/art/logging/manager/LoggingManager.java
+++ logging/src/main/java/io/art/logging/manager/LoggingManager.java
@@ -21,7 +21,10 @@ package io.art.logging.manager;
 import io.art.logging.configuration.*;
 import io.art.logging.state.*;
 import lombok.*;
+import static com.google.common.base.Throwables.*;
+import static io.art.scheduler.constants.SchedulerModuleConstants.Defaults.*;
 import static java.lang.Thread.*;
+import static java.util.concurrent.TimeUnit.*;
 
 @RequiredArgsConstructor
 public class LoggingManager {
@@ -38,16 +41,29 @@ public class LoggingManager {
 
     public LoggingManager deactivate() {
         if (!activated) return this;
-        configuration.getProducingExecutor().shutdown();
-        activated = false;
-        configuration.getConsumingExecutor().shutdown();
+        try {
+            configuration.getProducingExecutor().shutdown();
+            if (!configuration.getProducingExecutor().awaitTermination(DEFAULT_TERMINATION_TIMEOUT.getSeconds(), SECONDS)) {
+                return this;
+            }
+            activated = false;
+            configuration.getConsumingExecutor().shutdown();
+            if (!configuration.getConsumingExecutor().awaitTermination(DEFAULT_TERMINATION_TIMEOUT.getSeconds(), SECONDS)) {
+                return this;
+            }
+        } catch (Throwable throwable) {
+            System.err.println(getStackTraceAsString(throwable));
+        }
         return this;
     }
 
     private void processConsuming(LoggingModuleState state) {
         for (; ; ) {
             if (interrupted()) return;
-            if (!activated && state.all(loggerState -> loggerState.getQueue().isEmpty())) {
+            if (!activated) {
+                while (!state.all(loggerState -> loggerState.getQueue().isEmpty())) {
+                    state.forEach(loggerState -> loggerState.getConsumer().consume());
+                }
                 return;
             }
             state.forEach(loggerState -> loggerState.getConsumer().consume());
