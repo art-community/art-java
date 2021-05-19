41d19e2ec48dda367bb2bc4f4d5b2577671059c8
anton.bashirov <anton.sh.local@gmail.com>
2021-05-20 01:12:56 +0300

    ART

--- logging/build.gradle.kts
+++ logging/build.gradle.kts
@@ -17,11 +17,13 @@
  */
 
 dependencies {
+    val log4jVersion: String by project
     val sl4jVersion: String by project
     val jansiVersion: String by project
 
     implementation(project(":core"))
 
     api("org.slf4j", "slf4j-api", sl4jVersion)
+    api("org.apache.logging.log4j", "log4j-api", log4jVersion)
     api("org.fusesource.jansi", "jansi", jansiVersion)
 }
