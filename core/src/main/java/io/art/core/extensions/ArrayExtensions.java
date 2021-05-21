78fc3ca2575f3ed7cfd9efd0694cfbd015d320c3
anton.bashirov <anton.sh.local@gmail.com>
2021-05-21 12:55:33 +0300

    ART

--- core/src/main/java/io/art/core/extensions/ArrayExtensions.java
+++ core/src/main/java/io/art/core/extensions/ArrayExtensions.java
@@ -28,11 +28,11 @@ import static java.util.Objects.*;
 
 @UtilityClass
 public class ArrayExtensions {
-    public static <T> T[] concatArrays(T[] a1, T[] a2) {
-        if (isNull(a1) || a1.length == 0) return a2;
-        if (isNull(a2) || a2.length == 0) return a1;
-        T[] res = copyOf(a1, a1.length + a2.length);
-        arraycopy(a2, 0, res, a1.length, a2.length);
+    public static <T> T[] concatArrays(T[] first, T[] second) {
+        if (isNull(first) || first.length == 0) return second;
+        if (isNull(second) || second.length == 0) return first;
+        T[] res = copyOf(first, first.length + second.length);
+        arraycopy(second, 0, res, first.length, second.length);
         return res;
     }
 
