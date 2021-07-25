7f1e4d53fdb34f0463fc42b98c058c0a0c2123e3
anton.bashirov <anton.sh.local@gmail.com>
2021-07-25 23:25:07 +0300

    ART

--- communicator/src/main/java/io/art/communicator/proxy/CommunicatorProxyFactory.java
+++ communicator/src/main/java/io/art/communicator/proxy/CommunicatorProxyFactory.java
@@ -8,22 +8,33 @@ import static io.art.core.caster.Caster.*;
 import static io.art.core.collector.MapCollector.*;
 import static java.util.Objects.*;
 import static java.util.function.Function.*;
+import java.util.*;
 import java.util.function.*;
 
 @UtilityClass
 public class CommunicatorProxyFactory {
     public static <T> T communicatorProxy(MetaClass<T> proxyClass, Function<MetaMethod<?>, CommunicatorAction> provider) {
         Function<CommunicatorAction, Object> noArguments = CommunicatorAction::communicate;
-        BiFunction<CommunicatorAction, Object, Object> oneArguments = CommunicatorAction::communicate;
-        MetaProxy proxy = proxyClass.proxy(proxyClass.methods()
+        BiFunction<CommunicatorAction, Object, Object> oneArgument = CommunicatorAction::communicate;
+
+        Map<MetaMethod<?>, CommunicatorAction> actions = proxyClass.methods()
                 .stream()
                 .filter(method -> method.parameters().size() < 2)
-                .collect(mapCollector(identity(), method -> method.parameters().isEmpty()
-                        ? input -> noArguments.apply(provider.apply(method))
-                        : input -> oneArguments.apply(provider.apply(method), input))));
+                .collect(mapCollector(identity(), provider));
+
+        Map<MetaMethod<?>, Function<Object, Object>> invocations = actions
+                .entrySet()
+                .stream()
+                .collect(mapCollector(Map.Entry::getKey, entry -> entry.getKey().parameters().isEmpty()
+                        ? input -> noArguments.apply(entry.getValue())
+                        : input -> oneArgument.apply(entry.getValue(), input)));
+
+        MetaProxy proxy = proxyClass.proxy(invocations);
+
         if (isNull(proxy)) {
             throw new ImpossibleSituationException();
         }
+
         return cast(proxy);
     }
 }
