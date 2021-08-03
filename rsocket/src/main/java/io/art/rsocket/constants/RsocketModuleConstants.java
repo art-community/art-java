5b004d7ea9900d582cee5be6531ff4c8846ad834
anton.bashirov <anton.sh.local@gmail.com>
2021-08-04 00:32:54 +0300

    ART

--- rsocket/src/main/java/io/art/rsocket/constants/RsocketModuleConstants.java
+++ rsocket/src/main/java/io/art/rsocket/constants/RsocketModuleConstants.java
@@ -41,7 +41,7 @@ public interface RsocketModuleConstants {
         String SERVER_STARTED = "RSocket server started: {0}";
         String SERVER_STOPPED = "RSocket server stopped";
         String COMMUNICATOR_STARTED = "RSocket communicator started\nConnector: {0}\nSetup payload: {1}";
-        String COMMUNICATOR_STOPPED = "RSocket communicator stopped\nConnector: {0}\nSetup payload: {1}";
+        String COMMUNICATOR_STOPPED = "RSocket communicator stopped\nConnector: {0}";
 
         String FIRE_AND_FORGET_REQUEST_LOG = "RSocket executing fireAndForget()\nData:\n{0}\nMetadata:\n{1}";
         String FIRE_AND_FORGET_RESPONSE_LOG = "RSocket fireAndForget() completed";
