3cbe3318fb2808f12258b16f77faeec0241ba5d6
anton.bashirov <anton.sh.local@gmail.com>
2021-08-11 05:52:59 +0300

    ART

--- rsocket/src/test/java/io/art/rsocket/test/RsocketTest.java
+++ rsocket/src/test/java/io/art/rsocket/test/RsocketTest.java
@@ -1,26 +1,28 @@
 package io.art.rsocket.test;
 
-import io.art.core.extensions.*;
+import io.art.communicator.configurator.*;
 import io.art.meta.test.meta.*;
 import io.art.rsocket.test.communicator.*;
 import io.art.rsocket.test.meta.*;
 import io.art.rsocket.test.service.*;
+import io.art.server.configurator.*;
 import io.rsocket.core.*;
 import io.rsocket.transport.netty.server.*;
 import org.junit.jupiter.api.*;
 import reactor.core.publisher.*;
 import static io.art.core.initializer.Initializer.*;
+import static io.art.core.wrapper.ExceptionWrapper.*;
 import static io.art.json.module.JsonActivator.*;
 import static io.art.logging.module.LoggingActivator.*;
 import static io.art.meta.module.MetaActivator.*;
 import static io.art.rsocket.Rsocket.*;
 import static io.art.rsocket.module.RsocketActivator.*;
 import static io.rsocket.SocketAcceptor.*;
-import static io.rsocket.lease.Lease.create;
-import static java.time.Duration.*;
-import static reactor.core.publisher.Flux.*;
+import java.util.concurrent.*;
 
 public class RsocketTest {
+    private final static CountDownLatch waiter = new CountDownLatch(4);
+
     @BeforeAll
     public static void setup() {
         initialize(
@@ -30,33 +32,34 @@ public class RsocketTest {
                 rsocket(rsocket -> rsocket
                         .communicator(communicator -> communicator
                                 .tcp(TestRsocketConnector1.class, configurator -> configurator
-                                        .configure(connector -> connector.decorator(RSocketConnector::lease))
+                                        .configure(connector -> connector.logging(true))
                                         .roundRobin(builder -> builder
                                                 .client(client -> client.port(1234))
-                                                .client(client -> client.port(5678)))))
+                                                .client(client -> client.port(5678))))
+                                .configureCommunicator(TestRsocket1.class, CommunicatorActionConfigurator::logging))
                         .server(server -> server
                                 .tcp()
                                 .http(http -> http.port(9001))
-                                .configureService(TestRsocketService.class)
-                                .configureService(TestRsocketService1.class)
+                                .configureService(TestRsocketService.class, ServiceMethodConfigurator::logging)
+                                .configureService(TestRsocketService1.class, ServiceMethodConfigurator::logging)
                         )
                 )
         );
 
         RSocketServer
                 .create(forFireAndForget(payload -> {
+                    waiter.countDown();
                     System.out.println("1234");
                     return Mono.empty();
                 }))
-                .lease(spec -> spec.sender(() -> interval(ofSeconds(1)).map(ignore -> create(ofSeconds(1), 1))))
                 .bindNow(TcpServerTransport.create(1234));
 
         RSocketServer
                 .create(forFireAndForget(payload -> {
+                    waiter.countDown();
                     System.out.println("5678");
                     return Mono.empty();
                 }))
-                .lease(spec -> spec.sender(() -> interval(ofSeconds(1)).map(ignore -> create(ofSeconds(1), 1))))
                 .bindNow(TcpServerTransport.create(5678));
     }
 
@@ -67,6 +70,6 @@ public class RsocketTest {
         tcp.testRsocket1().m1("test");
         tcp.testRsocket1().m2("test");
         tcp.testRsocket1().m2("test");
-        ThreadExtensions.block();
+        ignoreException(waiter::await);
     }
 }
