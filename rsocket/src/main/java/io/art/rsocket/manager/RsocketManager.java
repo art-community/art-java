f41b80d796e106203f74ac95bbca9ef27fb5e0b2
anton.bashirov <anton.sh.local@gmail.com>
2021-08-14 22:52:51 +0300

    ART

--- rsocket/src/main/java/io/art/rsocket/manager/RsocketManager.java
+++ rsocket/src/main/java/io/art/rsocket/manager/RsocketManager.java
@@ -28,11 +28,12 @@ import io.art.rsocket.server.*;
 import lombok.*;
 import reactor.core.*;
 import static io.art.core.wrapper.ExceptionWrapper.*;
+import static io.art.logging.Logging.*;
 import static lombok.AccessLevel.*;
 
 public class RsocketManager {
     @Getter(lazy = true, value = PRIVATE)
-    private static final Logger logger = Logging.logger(RsocketManager.class);
+    private static final Logger logger = logger(RsocketManager.class);
 
     private final RsocketServer server;
     private final RsocketModuleConfiguration configuration;
