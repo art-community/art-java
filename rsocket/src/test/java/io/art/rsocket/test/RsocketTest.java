d3b7db0eefd20e9080227ca0282559d9c39cb744
anton.bashirov <anton.sh.local@gmail.com>
2021-08-19 23:09:00 +0300

    ART

--- rsocket/src/test/java/io/art/rsocket/test/RsocketTest.java
+++ rsocket/src/test/java/io/art/rsocket/test/RsocketTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.*;
 import reactor.core.publisher.*;
 import static io.art.core.extensions.ReactiveExtensions.*;
 import static io.art.core.initializer.Initializer.*;
-import static io.art.logging.module.LoggingActivator.*;
 import static io.art.message.pack.module.MessagePackActivator.*;
 import static io.art.meta.module.MetaActivator.*;
 import static io.art.rsocket.Rsocket.*;
@@ -24,7 +23,6 @@ public class RsocketTest {
     public static void setup() {
         initialize(
                 meta(() -> new MetaRsocketTest(new MetaMetaTest())),
-                logging(),
                 transport(),
                 messagePack(),
                 rsocket(rsocket -> rsocket
