5e1fab57b7a0790b0b37eccf57d52ac05e5a34bc
anton.bashirov <anton.sh.local@gmail.com>
2021-09-19 02:13:56 +0300

    ART

--- http/src/main/java/io/art/http/state/WsLocalState.java
+++ http/src/main/java/io/art/http/state/WsLocalState.java
@@ -2,6 +2,7 @@ package io.art.http.state;
 
 import io.art.core.annotation.*;
 import io.art.http.configuration.*;
+import io.netty.handler.codec.http.*;
 import lombok.*;
 import lombok.experimental.*;
 import reactor.netty.http.websocket.*;
@@ -10,11 +11,18 @@ import static lombok.AccessLevel.*;
 @Getter
 @Public
 @Accessors(fluent = true)
-@AllArgsConstructor(access = PRIVATE)
 public class WsLocalState {
     private final WebsocketInbound inbound;
     private final WebsocketOutbound outbound;
     private final HttpRouteConfiguration routeConfiguration;
+    private final HttpHeaders requestHeaders;
+
+    private WsLocalState(WebsocketInbound inbound, WebsocketOutbound outbound, HttpRouteConfiguration routeConfiguration) {
+        this.inbound = inbound;
+        this.outbound = outbound;
+        this.routeConfiguration = routeConfiguration;
+        requestHeaders = inbound.headers();
+    }
 
     public static WsLocalState wsLocalState(WebsocketInbound inbound, WebsocketOutbound outbound, HttpRouteConfiguration routeConfiguration) {
         return new WsLocalState(inbound, outbound, routeConfiguration);
