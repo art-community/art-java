c82a359feb8a2ece6f6ec347e05bf54f8e298542
anton.bashirov <anton.sh.local@gmail.com>
2021-10-10 20:25:44 +0300

    ART

--- http/src/main/java/io/art/http/Http.java
+++ http/src/main/java/io/art/http/Http.java
@@ -18,4 +18,8 @@ public class Http {
     public <C, M extends MetaClass<C>> HttpLocalState httpState(Class<C> owner, Function<M, MetaMethod<?>> method) {
         return httpModule().state().httpState(owner, method);
     }
+
+    public <C, M extends MetaClass<C>> WsLocalState wsState(Class<C> owner, Function<M, MetaMethod<?>> method) {
+        return httpModule().state().wsState(owner, method);
+    }
 }
