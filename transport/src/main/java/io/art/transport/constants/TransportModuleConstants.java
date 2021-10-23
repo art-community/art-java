f255ad0899b5f73e9253489cdf74639049a47c36
anton.bashirov <anton.sh.local@gmail.com>
2021-10-24 00:19:21 +0300

    ART

--- transport/src/main/java/io/art/transport/constants/TransportModuleConstants.java
+++ transport/src/main/java/io/art/transport/constants/TransportModuleConstants.java
@@ -25,7 +25,7 @@ public interface TransportModuleConstants {
         Duration DEFAULT_MAX_LIFE_TIME = ofMillis(-1);
         Duration DEFAULT_MAX_IDLE_TIME = ofMillis(-1);
         String DEFAULT_LEASING_STRATEGY = LEASING_STRATEGY_FIFO;
-        BufferType DEFAULT_BUFFER_TYPE = IO;
+        BufferType DEFAULT_BUFFER_TYPE = DIRECT;
         int DEFAULT_BUFFER_INITIAL_CAPACITY = 256;
         int DEFAULT_BUFFER_MAX_CAPACITY = MAX_VALUE;
     }
