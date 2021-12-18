289f221dc42a893eb854b76c81f9ab68f8c922ca
anton.bashirov <anton.sh.local@gmail.com>
2021-12-19 02:59:12 +0300

    ART

--- fibers/src/main/java/io/art/fibers/Fibers.java
+++ fibers/src/main/java/io/art/fibers/Fibers.java
@@ -1,29 +1,41 @@
 package io.art.fibers;
 
 import static io.art.fibers.service.FiberService.*;
-import java.time.*;
+import java.util.concurrent.atomic.*;
 
 public class Fibers {
-    private static boolean done = false;
-    private static LocalDateTime start = LocalDateTime.now();
+    private final static int iterations = 5000000;
+    private static final AtomicInteger counter = new AtomicInteger();
 
     public static void main(String[] args) {
-        createFiber(Fibers::test1);
-        while (!done) {
-            if (LocalDateTime.now().isAfter(start.plusSeconds(300))) {
-                done = true;
-            }
+        long begin = System.nanoTime();
+
+        createFiber(Fibers::cofunc1);
+
+        long end = System.nanoTime();
+
+        int num_switches = iterations * 2;
+        double time_ns = (end - begin) + (end - begin) * 1000000000L;
+        double time_s = time_ns * 1e-9;
+        double switches_per_second = num_switches / time_s;
+        double ns_per_switch = time_ns / num_switches;
+
+        System.out.println("Nano seconds per switch: " + ns_per_switch);
+        System.out.println("Switches per second: " + switches_per_second);
+
+        destroyFiber();
+    }
+
+    static void cofunc1() {
+        cofunc2();
+        while (counter.get() < iterations) {
             suspend();
         }
     }
 
-    public static void test1() {
-        while (!done) {
+    static void cofunc2() {
+        while (counter.incrementAndGet() < iterations) {
             suspend();
         }
-    }
-
-    public static void test2() {
-        System.out.println("input 2");
     }
 }
