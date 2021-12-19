200db2cf67836cb76c8509478a405064d681187e
anton.bashirov <anton.sh.local@gmail.com>
2021-12-19 03:19:50 +0300

    ART

--- fibers/src/main/java/io/art/fibers/Fibers.java
+++ fibers/src/main/java/io/art/fibers/Fibers.java
@@ -17,6 +17,7 @@ public class Fibers {
         long begin = System.nanoTime();
 
         while (counter.get() < iterations) {
+            System.out.println("resume");
             coroutine_resume(GraalCoroutineService.current.get().coroutine, WordFactory.nullPointer());
         }
 
@@ -36,6 +37,7 @@ public class Fibers {
 
     static void cofunc() {
         while (counter.incrementAndGet() < iterations) {
+            System.out.println("yield");
             coroutine_yield(WordFactory.nullPointer());
         }
     }
