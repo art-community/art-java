04d0ea726cbf2240efbf5b1ad968a5dd8d007e2e
anton.bashirov <anton.sh.local@gmail.com>
2022-01-16 00:25:34 +0300

    ART

--- storage/src/main/java/io/art/storage/Space.java
+++ storage/src/main/java/io/art/storage/Space.java
@@ -5,27 +5,5 @@ import io.art.core.annotation.*;
 
 @Public
 public interface Space extends Communicator {
-    default <T, K> T get(K key) {
-        return null;
-    }
 
-    default <T, K> T insert(T data) {
-        return null;
-    }
-
-    default <T, K> T put(T data) {
-        return null;
-    }
-
-    default <T, K> T delete(K key) {
-        return null;
-    }
-
-    default void truncate() {
-
-    }
-
-    default Long count() {
-        return null;
-    }
 }
