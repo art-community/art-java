58098d323c0af2167f40d61191a7eba414c5ce9d
anton.bashirov <anton.sh.local@gmail.com>
2022-01-22 05:44:16 +0300

    ART

--- communicator/src/main/java/io/art/communicator/factory/CommunicatorActionFactory.java
+++ communicator/src/main/java/io/art/communicator/factory/CommunicatorActionFactory.java
@@ -1,5 +1,6 @@
 package io.art.communicator.factory;
 
+import io.art.communicator.*;
 import io.art.communicator.action.*;
 import io.art.communicator.action.CommunicatorAction.*;
 import io.art.communicator.configuration.*;
@@ -21,38 +22,36 @@ import static java.util.Objects.*;
 
 @UtilityClass
 public class CommunicatorActionFactory {
-    public CommunicatorAction communicatorAction(MetaClass<?> owner, MetaMethod<?> method, Communication communication) {
-        return communicatorAction(communicatorActionId(idByDash(owner.definition().type()), method.name()), method, communication);
-    }
-
-    public CommunicatorAction communicatorAction(CommunicatorActionIdentifier id, MetaMethod<?> method, Communication communication) {
+    public CommunicatorAction communicatorAction(MetaClass<? extends Communicator> owner, MetaMethod<?> method, Communication communication) {
         MetaType<?> inputType = orNull(() -> immutableArrayOf(method.parameters().values()).get(0).type(), isNotEmpty(method.parameters()));
         CommunicatorActionBuilder builder = CommunicatorAction.builder()
-                .id(id)
+                .id(communicatorActionId(idByDash(owner.definition().type()), method.name()))
                 .outputType(method.returnType())
-                .communication(communication);
+                .communication(communication)
+                .method(method)
+                .owner(owner);
         if (nonNull(inputType)) {
             return builder.inputType(inputType).build();
         }
         return builder.build();
     }
 
-    public CommunicatorAction preconfiguredCommunicatorAction(MetaClass<?> owner, MetaMethod<?> method, Communication communication) {
-        return preconfiguredCommunicatorAction(communicatorActionId(idByDash(owner.definition().type()), method.name()), method, communication);
-    }
-
-    public CommunicatorAction preconfiguredCommunicatorAction(CommunicatorActionIdentifier id, MetaMethod<?> method, Communication communication) {
+    public CommunicatorAction preconfiguredCommunicatorAction(MetaClass<? extends Communicator> owner, MetaMethod<?> method, Communication communication) {
+        CommunicatorActionIdentifier id = communicatorActionId(idByDash(owner.definition().type()), method.name());
         MetaType<?> inputType = orNull(() -> immutableArrayOf(method.parameters().values()).get(0).type(), isNotEmpty(method.parameters()));
         CommunicatorConfiguration configuration = communicatorConfiguration(new CommunicatorRefresher());
         CommunicatorActionBuilder builder = CommunicatorAction.builder().id(id).outputType(method.returnType()).communication(communication);
         if (withLogging()) {
             builder
                     .inputDecorator(new CommunicatorLoggingDecorator(id, configuration, INPUT))
-                    .outputDecorator(new CommunicatorLoggingDecorator(id, configuration, OUTPUT));
+                    .outputDecorator(new CommunicatorLoggingDecorator(id, configuration, OUTPUT))
+                    .method(method)
+                    .owner(owner);
         }
         if (nonNull(inputType)) {
             return builder.inputType(inputType).build();
         }
         return builder.build();
     }
+
 }
