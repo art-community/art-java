052f6d050fb28530f1b84573b3c8e418deed4b9e
anton.bashirov <anton.sh.local@gmail.com>
2022-02-03 02:47:53 +0300

    ART

--- tarantool/src/main/java/io/art/tarantool/service/TarantoolSpaceService.java
+++ tarantool/src/main/java/io/art/tarantool/service/TarantoolSpaceService.java
@@ -36,6 +36,25 @@ public class TarantoolSpaceService<KeyType, ValueType> {
         return reactive.findAll(keys).toStream().collect(immutableArrayCollector());
     }
 
+
+    public ValueType delete(KeyType key) {
+        return block(reactive.delete(key));
+    }
+
+    @SafeVarargs
+    public final ImmutableArray<ValueType> delete(KeyType... keys) {
+        return delete(asList(keys));
+    }
+
+    public ImmutableArray<ValueType> delete(Collection<KeyType> keys) {
+        return reactive.delete(keys).toStream().collect(immutableArrayCollector());
+    }
+
+    public ImmutableArray<ValueType> delete(ImmutableCollection<KeyType> keys) {
+        return reactive.delete(keys).toStream().collect(immutableArrayCollector());
+    }
+
+
     public long count() {
         return block(reactive.count());
     }
@@ -44,21 +63,60 @@ public class TarantoolSpaceService<KeyType, ValueType> {
         reactive.truncate();
     }
 
+
     public ValueType insert(ValueType value) {
         return block(reactive.insert(value));
     }
 
+    @SafeVarargs
+    public final ImmutableArray<ValueType> insert(ValueType... value) {
+        return reactive.insert(value).toStream().collect(immutableArrayCollector());
+    }
+
+    public ImmutableArray<ValueType> insert(Collection<ValueType> value) {
+        return reactive.insert(value).toStream().collect(immutableArrayCollector());
+    }
+
+    public ImmutableArray<ValueType> insert(ImmutableCollection<ValueType> value) {
+        return reactive.insert(value).toStream().collect(immutableArrayCollector());
+    }
+
+
     public ValueType put(ValueType value) {
         return block(reactive.put(value));
+    }
+
+    @SafeVarargs
+    public final ImmutableArray<ValueType> put(ValueType... value) {
+        return reactive.put(value).toStream().collect(immutableArrayCollector());
     }
 
     public ImmutableArray<ValueType> put(Collection<ValueType> value) {
         return reactive.put(value).toStream().collect(immutableArrayCollector());
     }
 
-    public ValueType replace(ValueType value) {
-        return put(value);
+    public ImmutableArray<ValueType> put(ImmutableCollection<ValueType> value) {
+        return reactive.put(value).toStream().collect(immutableArrayCollector());
     }
+
+
+    public ValueType replace(ValueType value) {
+        return block(reactive.put(value));
+    }
+
+    @SafeVarargs
+    public final ImmutableArray<ValueType> replace(ValueType... value) {
+        return reactive.put(value).toStream().collect(immutableArrayCollector());
+    }
+
+    public ImmutableArray<ValueType> replace(Collection<ValueType> value) {
+        return reactive.put(value).toStream().collect(immutableArrayCollector());
+    }
+
+    public ImmutableArray<ValueType> replace(ImmutableCollection<ValueType> value) {
+        return reactive.put(value).toStream().collect(immutableArrayCollector());
+    }
+
 
     public TarantoolReactiveSpaceService<KeyType, ValueType> reactive() {
         return reactive;
