33d0abc1d0d09d134a905b7a828abf2da20a0015
Anton Bashirov <anton.bashirov@rt.ru>
2022-02-15 17:15:13 +0300

    ART

--- tarantool/src/test/java/io/art/tarantool/test/TarantoolStorageTest.java
+++ tarantool/src/test/java/io/art/tarantool/test/TarantoolStorageTest.java
@@ -1,5 +1,6 @@
 package io.art.tarantool.test;
 
+import io.art.core.collection.*;
 import io.art.meta.test.*;
 import io.art.meta.test.meta.*;
 import io.art.storage.*;
@@ -8,6 +9,7 @@ import io.art.tarantool.test.meta.*;
 import io.art.tarantool.test.model.*;
 import org.junit.jupiter.api.*;
 import static io.art.core.context.Context.*;
+import static io.art.core.factory.ArrayFactory.*;
 import static io.art.core.initializer.Initializer.*;
 import static io.art.meta.module.MetaActivator.*;
 import static io.art.meta.test.TestingMetaModelGenerator.*;
@@ -18,6 +20,8 @@ import static io.art.tarantool.module.TarantoolActivator.*;
 import static io.art.tarantool.test.constants.TestTarantoolConstants.*;
 import static io.art.tarantool.test.manager.TestTarantoolInstanceManager.*;
 import static io.art.transport.module.TransportActivator.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
 
 public class TarantoolStorageTest {
     @BeforeAll
@@ -51,6 +55,11 @@ public class TarantoolStorageTest {
         shutdown();
     }
 
+    @AfterEach
+    public void truncate() {
+        space().truncate();
+    }
+
     @Test
     public void testSinglePut() {
         TestingMetaModel data = generateTestingModel();
@@ -58,10 +67,101 @@ public class TarantoolStorageTest {
     }
 
     @Test
+    public void testMultiplePut() {
+        List<TestingMetaModel> data = fixedArrayOf(
+                generateTestingModel().toBuilder().f1(1).build(),
+                generateTestingModel().toBuilder().f1(2).build(),
+                generateTestingModel().toBuilder().f1(3).build()
+        );
+        ImmutableArray<TestingMetaModel> result = space().put(data);
+        assertEquals(data.size(), result.size());
+        data.get(0).assertEquals(result.get(0));
+        data.get(1).assertEquals(result.get(1));
+        data.get(2).assertEquals(result.get(2));
+    }
+
+    @Test
+    public void testSingleInsert() {
+        TestingMetaModel data = generateTestingModel();
+        data.assertEquals(space().insert(data));
+    }
+
+    @Test
+    public void testMultipleInsert() {
+        List<TestingMetaModel> data = fixedArrayOf(
+                generateTestingModel().toBuilder().f1(1).build(),
+                generateTestingModel().toBuilder().f1(2).build(),
+                generateTestingModel().toBuilder().f1(3).build()
+        );
+        ImmutableArray<TestingMetaModel> result = space().insert(data);
+        assertEquals(data.size(), result.size());
+        data.get(0).assertEquals(result.get(0));
+        data.get(1).assertEquals(result.get(1));
+        data.get(2).assertEquals(result.get(2));
+    }
+
+    @Test
+    public void testSingleDelete() {
+        TestingMetaModel data = generateTestingModel();
+        space().insert(data);
+        space().delete(data.getF1());
+        assertEquals(0, space().count());
+    }
+
+    @Test
+    public void testMultipleDelete() {
+        List<TestingMetaModel> data = fixedArrayOf(
+                generateTestingModel().toBuilder().f1(1).build(),
+                generateTestingModel().toBuilder().f1(2).build(),
+                generateTestingModel().toBuilder().f1(3).build()
+        );
+        space().insert(data);
+        space().delete(1, 2, 3);
+        assertEquals(0, space().count());
+    }
+
+    @Test
+    public void testTruncate() {
+        List<TestingMetaModel> data = fixedArrayOf(
+                generateTestingModel().toBuilder().f1(1).build(),
+                generateTestingModel().toBuilder().f1(2).build(),
+                generateTestingModel().toBuilder().f1(3).build()
+        );
+        space().insert(data);
+        space().truncate();
+        assertEquals(0, space().count());
+    }
+
+    @Test
+    public void testCount() {
+        List<TestingMetaModel> data = fixedArrayOf(
+                generateTestingModel().toBuilder().f1(1).build(),
+                generateTestingModel().toBuilder().f1(2).build(),
+                generateTestingModel().toBuilder().f1(3).build()
+        );
+        space().insert(data);
+        assertEquals(3, space().count());
+    }
+
+    @Test
     public void testFindFirst() {
         TestingMetaModel data = generateTestingModel();
         space().put(data);
         data.assertEquals(space().findFirst(1));
+    }
+
+    @Test
+    public void testFindAll() {
+        List<TestingMetaModel> data = fixedArrayOf(
+                generateTestingModel().toBuilder().f1(1).build(),
+                generateTestingModel().toBuilder().f1(2).build(),
+                generateTestingModel().toBuilder().f1(3).build()
+        );
+        ImmutableArray<TestingMetaModel> result = space().findAll(1, 2, 3);
+        assertEquals(data.size(), result.size());
+        data.get(0).assertEquals(result.get(0));
+        data.get(1).assertEquals(result.get(1));
+        data.get(2).assertEquals(result.get(2));
     }
 
     private static SpaceService<Integer, TestingMetaModel> space() {
