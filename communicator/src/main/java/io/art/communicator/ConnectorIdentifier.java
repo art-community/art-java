ec6dbb89cb2a3d272acc5f9ba6b686dce2647307
anton.bashirov <anton.sh.local@gmail.com>
2022-03-01 02:27:44 +0300

    ART

--- communicator/src/main/java/io/art/communicator/ConnectorIdentifier.java
+++ communicator/src/main/java/io/art/communicator/ConnectorIdentifier.java
@@ -1,7 +1,9 @@
 package io.art.communicator;
 
+import io.art.core.annotation.*;
 import static io.art.core.constants.StringConstants.*;
 
+@Public
 @FunctionalInterface
 public interface ConnectorIdentifier {
     String name();
