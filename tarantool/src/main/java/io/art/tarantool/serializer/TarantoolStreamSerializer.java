f7a8d5bf0046f691e440115226680ea5b12173a8
Anton Bashirov <anton.bashirov@rt.ru>
2022-03-11 14:36:46 +0300

    ART

---
+++ tarantool/src/main/java/io/art/tarantool/serializer/TarantoolStreamSerializer.java
@@ -0,0 +1,101 @@
+package io.art.tarantool.serializer;
+
+import io.art.core.exception.*;
+import io.art.meta.model.*;
+import io.art.storage.constants.*;
+import io.art.storage.filter.implementation.*;
+import io.art.storage.mapper.*;
+import io.art.storage.sorter.implementation.*;
+import io.art.storage.stream.*;
+import io.art.tarantool.descriptor.*;
+import lombok.experimental.*;
+import org.msgpack.value.*;
+import static io.art.core.caster.Caster.*;
+import static io.art.core.collector.ArrayCollector.*;
+import static io.art.core.factory.ListFactory.*;
+import static io.art.meta.registry.BuiltinMetaTypes.*;
+import static io.art.tarantool.constants.TarantoolModuleConstants.*;
+import static org.msgpack.value.ValueFactory.*;
+import java.util.*;
+
+@UtilityClass
+public class TarantoolStreamSerializer {
+    public static List<Value> serializeStream(TarantoolModelWriter writer, List<ProcessingOperator> operators) {
+        List<Value> serialized = linkedList();
+        for (ProcessingOperator operator : operators) {
+            switch (operator.getOperation()) {
+                case LIMIT:
+                    serialized.add(newArray(STREAM_PROTOCOL.processingFunctions.processingLimit, serializeValue(writer, longPrimitiveType(), operator.getValue())));
+                    break;
+                case OFFSET:
+                    serialized.add(newArray(STREAM_PROTOCOL.processingFunctions.processingOffset, serializeValue(writer, longPrimitiveType(), operator.getValue())));
+                    break;
+                case DISTINCT:
+                    serialized.add(newArray(STREAM_PROTOCOL.processingFunctions.processingDistinct));
+                    break;
+                case SORT:
+                    serialized.add(newArray(STREAM_PROTOCOL.processingFunctions.processingSort, serializeSort(cast(operator.getValue()))));
+                    break;
+                case FILTER:
+                    serialized.add(newArray(STREAM_PROTOCOL.processingFunctions.processingFilter, serializeFilter(cast(operator.getValue()))));
+                    break;
+                case MAP:
+                    serialized.add(newArray(STREAM_PROTOCOL.processingFunctions.processingMap, serializeMap(cast(operator.getValue()))));
+                    break;
+            }
+        }
+        return serialized;
+    }
+
+    private static ImmutableArrayValue serializeSort(SorterImplementation<?, ?> sorter) {
+        StorageConstants.SortOrder order = sorter.getOrder();
+        MetaField<?, ?> field = sorter.getField();
+        switch (order) {
+            case ASCENDANT:
+                return newArray(STREAM_PROTOCOL.comparators.comparatorLess, newInteger(field.index() + 1));
+            case DESCENDANT:
+                return newArray(STREAM_PROTOCOL.comparators.comparatorMore, newInteger(field.index() + 1));
+        }
+        throw new ImpossibleSituationException();
+    }
+
+    public static ImmutableArrayValue serializeFilter(FilterImplementation<?> filter) {
+        List<ImmutableArrayValue> serialized = linkedList();
+        List<FilterImplementation.FilterPart> parts = filter.getParts();
+        for (FilterImplementation.FilterPart part : parts) {
+            StorageConstants.FilterCondition condition = part.getCondition();
+            switch (part.getMode()) {
+                case FIELD:
+                    part.getByField();
+                    break;
+                case FUNCTION:
+                    FilterByFunctionImplementation<?> byFunction = part.getByFunction();
+                    break;
+                case SPACE:
+                    FilterBySpaceImplementation<?, ?> bySpace = part.getBySpace();
+
+                    break;
+                case INDEX:
+                    FilterBySpaceImplementation<?, ?> byIndex = part.getByIndex();
+                    break;
+                case NESTED:
+                    NestedFilter nested = part.getNested();
+                    break;
+            }
+        }
+        return newArray(serialized);
+    }
+
+    private static ImmutableArrayValue serializeMap(Mapper<?, ?> filter) {
+        return null;
+    }
+
+
+    private static ImmutableArrayValue serializeFilterValues(TarantoolModelWriter writer, MetaType<?> type, List<Object> values) {
+        return newArray(values.stream().map(value -> serializeValue(writer, type, value)).collect(listCollector()));
+    }
+
+    private static Value serializeValue(TarantoolModelWriter writer, MetaType<?> type, Object value) {
+        return writer.write(type, value);
+    }
+}
--- tarantool/src/main/java/io/art/tarantool/service/TarantoolReactiveStream.java
+++ tarantool/src/main/java/io/art/tarantool/service/TarantoolReactiveStream.java
@@ -22,6 +22,8 @@ import static io.art.tarantool.constants.TarantoolModuleConstants.Functions.*;
 import static io.art.tarantool.constants.TarantoolModuleConstants.ProcessingOptions.*;
 import static io.art.tarantool.constants.TarantoolModuleConstants.*;
 import static io.art.tarantool.constants.TarantoolModuleConstants.SortOptions.*;
+import static io.art.tarantool.serializer.TarantoolStreamSerializer.serializeFilter;
+import static io.art.tarantool.serializer.TarantoolStreamSerializer.serializeStream;
 import static org.msgpack.value.ValueFactory.*;
 import java.util.*;
 import java.util.function.*;
@@ -37,7 +39,7 @@ public class TarantoolReactiveStream<ModelType> extends ReactiveSpaceStream<Mode
         Mono<Value> result = service
                 .clients
                 .immutable()
-                .call(SPACE_STREAM, newArray(service.spaceName, newArray(serializeStream()), newArray(operator)));
+                .call(SPACE_STREAM, newArray(service.spaceName, newArray(serializeStream(service.writer, operators)), newArray(operator)));
         return service.parseSpaceFlux(result);
     }
 
@@ -47,7 +49,7 @@ public class TarantoolReactiveStream<ModelType> extends ReactiveSpaceStream<Mode
         Mono<Value> result = service
                 .clients
                 .immutable()
-                .call(SPACE_STREAM, newArray(service.spaceName, newArray(serializeStream()), newArray(operator)));
+                .call(SPACE_STREAM, newArray(service.spaceName, newArray(serializeStream(service.writer, operators)), newArray(operator)));
         return service.parseLongMono(result);
     }
 
@@ -59,7 +61,7 @@ public class TarantoolReactiveStream<ModelType> extends ReactiveSpaceStream<Mode
         Mono<Value> result = service
                 .clients
                 .immutable()
-                .call(SPACE_STREAM, newArray(service.spaceName, newArray(serializeStream()), newArray(operator, serializeFilter(newFilter))));
+                .call(SPACE_STREAM, newArray(service.spaceName, newArray(serializeStream(service.writer, operators)), newArray(operator, serializeFilter(newFilter))));
         return service.parseBooleanMono(result);
     }
 
@@ -71,7 +73,7 @@ public class TarantoolReactiveStream<ModelType> extends ReactiveSpaceStream<Mode
         Mono<Value> result = service
                 .clients
                 .immutable()
-                .call(SPACE_STREAM, newArray(service.spaceName, newArray(serializeStream()), newArray(operator, serializeFilter(newFilter))));
+                .call(SPACE_STREAM, newArray(service.spaceName, newArray(serializeStream(service.writer, operators)), newArray(operator, serializeFilter(newFilter))));
         return service.parseBooleanMono(result);
     }
 
@@ -83,86 +85,7 @@ public class TarantoolReactiveStream<ModelType> extends ReactiveSpaceStream<Mode
         Mono<Value> result = service
                 .clients
                 .immutable()
-                .call(SPACE_STREAM, newArray(service.spaceName, newArray(serializeStream()), newArray(operator, serializeFilter(newFilter))));
+                .call(SPACE_STREAM, newArray(service.spaceName, newArray(serializeStream(service.writer, operators)), newArray(operator, serializeFilter(newFilter))));
         return service.parseBooleanMono(result);
-    }
-
-    private List<Value> serializeStream() {
-        List<Value> serialized = linkedList();
-        for (ProcessingOperator operator : operators) {
-            switch (operator.getOperation()) {
-                case LIMIT:
-                    serialized.add(newArray(STREAM_PROTOCOL.processingFunctions.processingLimit, serializeValue(longPrimitiveType(), operator.getValue())));
-                    break;
-                case OFFSET:
-                    serialized.add(newArray(STREAM_PROTOCOL.processingFunctions.processingOffset, serializeValue(longPrimitiveType(), operator.getValue())));
-                    break;
-                case DISTINCT:
-                    serialized.add(newArray(STREAM_PROTOCOL.processingFunctions.processingDistinct));
-                    break;
-                case SORT:
-                    serialized.add(newArray(STREAM_PROTOCOL.processingFunctions.processingSort, serializeSort(cast(operator.getValue()))));
-                    break;
-                case FILTER:
-                    serialized.add(newArray(STREAM_PROTOCOL.processingFunctions.processingFilter, serializeFilter(cast(operator.getValue()))));
-                    break;
-                case MAP:
-                    serialized.add(newArray(STREAM_PROTOCOL.processingFunctions.processingMap, serializeMap(cast(operator.getValue()))));
-                    break;
-            }
-        }
-        return serialized;
-    }
-
-    private ImmutableArrayValue serializeSort(SorterImplementation<?, ?> sorter) {
-        SortOrder order = sorter.getOrder();
-        MetaField<?, ?> field = sorter.getField();
-        switch (order) {
-            case ASCENDANT:
-                return newArray(STREAM_PROTOCOL.comparators.comparatorLess, newInteger(field.index() + 1));
-            case DESCENDANT:
-                return newArray(STREAM_PROTOCOL.comparators.comparatorMore, newInteger(field.index() + 1));
-        }
-        throw new ImpossibleSituationException();
-    }
-
-    private ImmutableArrayValue serializeFilter(FilterImplementation<?> filter) {
-        List<ImmutableArrayValue> serialized = linkedList();
-        List<FilterPart> parts = filter.getParts();
-        for (FilterPart part : parts) {
-            FilterCondition condition = part.getCondition();
-            switch (part.getMode()) {
-                case FIELD:
-                    part.getByField();
-                    break;
-                case FUNCTION:
-                    FilterByFunctionImplementation<?> byFunction = part.getByFunction();
-                    break;
-                case SPACE:
-                    FilterBySpaceImplementation<?, ?> bySpace = part.getBySpace();
-
-                    break;
-                case INDEX:
-                    FilterBySpaceImplementation<?, ?> byIndex = part.getByIndex();
-                    break;
-                case NESTED:
-                    NestedFilter nested = part.getNested();
-                    break;
-            }
-        }
-        return newArray(serialized);
-    }
-
-    private ImmutableArrayValue serializeMap(Mapper<?, ?> filter) {
-
-    }
-
-
-    private ImmutableArrayValue serializeFilterValues(MetaType<?> type, List<Object> values) {
-        return newArray(values.stream().map(value -> serializeValue(type, value)).collect(listCollector()));
-    }
-
-    private Value serializeValue(MetaType<?> type, Object value) {
-        return service.writer.write(type, value);
     }
 }
