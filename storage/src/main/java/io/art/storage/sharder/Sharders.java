8c28fa4aa922d8378d4c1851398c048d651b6f81
anton.bashirov <anton.sh.local@gmail.com>
2022-03-18 16:24:20 +0300

    ART

--- storage/src/main/java/io/art/storage/sharder/Sharders.java
+++ storage/src/main/java/io/art/storage/sharder/Sharders.java
@@ -1,6 +1,7 @@
 package io.art.storage.sharder;
 
 import io.art.core.annotation.*;
+import io.art.core.model.*;
 import io.art.storage.constants.StorageConstants.*;
 import static io.art.core.model.Tuple.*;
 import static io.art.storage.constants.StorageConstants.ShardAlgorithm.*;
@@ -15,19 +16,51 @@ public interface Sharders<T> {
         return ignore -> sharder(constant, algorithm);
     }
 
-    default <P1> ShardRequest sharder(P1 p1) {
-        return sharder(p1, CRC_32);
+    default <P1> ShardRequest sharder(Tuple tuple) {
+        return sharder(CRC_32, tuple);
     }
 
-    default <P1> ShardRequest sharder(P1 p1, ShardAlgorithm algorithm) {
+    default <P1> ShardRequest sharder(ShardAlgorithm algorithm, Tuple tuple) {
+        return new ShardRequest(algorithm, tuple);
+    }
+
+    default <P1> ShardRequest sharder(P1 p1) {
+        return sharder(CRC_32, p1);
+    }
+
+    default <P1> ShardRequest sharder(ShardAlgorithm algorithm, P1 p1) {
         return new ShardRequest(algorithm, tuple(p1));
     }
 
     default <P1, P2> ShardRequest sharder(P1 p1, P2 p2) {
-        return sharder(p1, p2, CRC_32);
+        return sharder(CRC_32, p1, p2);
     }
 
-    default <P1, P2> ShardRequest sharder(P1 P1, P2 p2, ShardAlgorithm algorithm) {
+    default <P1, P2> ShardRequest sharder(ShardAlgorithm algorithm, P1 P1, P2 p2) {
         return new ShardRequest(algorithm, tuple(P1, p2));
+    }
+
+    default <P1, P2, P3> ShardRequest sharder(P1 p1, P2 p2, P3 p3) {
+        return sharder(CRC_32, p1, p2, p3);
+    }
+
+    default <P1, P2, P3> ShardRequest sharder(ShardAlgorithm algorithm, P1 P1, P2 p2, P3 p3) {
+        return new ShardRequest(algorithm, tuple(P1, p2, p3));
+    }
+
+    default <P1, P2, P3, P4> ShardRequest sharder(P1 p1, P2 p2, P3 p3, P4 p4) {
+        return sharder(CRC_32, p1, p2, p3, p4);
+    }
+
+    default <P1, P2, P3, P4> ShardRequest sharder(ShardAlgorithm algorithm, P1 P1, P2 p2, P3 p3, P4 p4) {
+        return new ShardRequest(algorithm, tuple(P1, p2, p3, p4));
+    }
+
+    default <P1, P2, P3, P4, P5> ShardRequest sharder(P1 p1, P2 p2, P3 p3, P4 p4, P5 p5) {
+        return sharder(CRC_32, p1, p2, p3, p4, p5);
+    }
+
+    default <P1, P2, P3, P4, P5> ShardRequest sharder(ShardAlgorithm algorithm, P1 P1, P2 p2, P3 p3, P4 p4, P5 p5) {
+        return new ShardRequest(algorithm, tuple(P1, p2, p3, p4, p5));
     }
 }
