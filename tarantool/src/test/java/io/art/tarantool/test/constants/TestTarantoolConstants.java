50e4762d92a0f4025e7b74cea4d0e306e642004e
anton.bashirov <anton.sh.local@gmail.com>
2022-03-20 02:47:20 +0300

    ART

--- tarantool/src/test/java/io/art/tarantool/test/constants/TestTarantoolConstants.java
+++ tarantool/src/test/java/io/art/tarantool/test/constants/TestTarantoolConstants.java
@@ -6,11 +6,30 @@ public interface TestTarantoolConstants {
     String STORAGE_DIRECTORY = "test-storage";
     String STORAGE_COMMAND = "cd " + convertToWslPath(STORAGE_DIRECTORY) + " && tarantool";
     String STORAGE_PID = "test-storage.pid";
+    String STORAGE_SCRIPT = "test-storage.lua";
+    int STORAGE_PORT = 3301;
+
+    String ROUTER_DIRECTORY = "test-router";
+    String ROUTER_COMMAND = "cd " + convertToWslPath(ROUTER_DIRECTORY) + " && tarantool";
+    String ROUTER_PID = "test-router.pid";
+    String ROUTER_SCRIPT = "test-router.lua";
+    int ROUTER_PORT = 3302;
+
+    String SHARD_1_DIRECTORY = "test-shard-1";
+    String SHARD_1_COMMAND = "cd " + convertToWslPath(SHARD_1_DIRECTORY) + " && tarantool";
+    String SHARD_1_PID = "test-shard-1.pid";
+    String SHARD_1_SCRIPT = "test-shard-1.lua";
+    int SHARD_1_PORT = 3303;
+
+    String SHARD_2_DIRECTORY = "test-shard-2";
+    String SHARD_2_COMMAND = "cd " + convertToWslPath(SHARD_2_DIRECTORY) + " && tarantool";
+    String SHARD_2_PID = "test-shard-2.pid";
+    String SHARD_2_SCRIPT = "test-shard-2.lua";
+    int SHARD_2_PORT = 3304;
+
     String KILL_COMMAND = "kill -9 ";
     String BASH = "bash";
     String BASH_ARGUMENT = "-c";
-    String STORAGE_SCRIPT = "test-storage.lua";
-    int STORAGE_PORT = 3301;
     String USERNAME = "username";
     String PASSWORD = "password";
     String MODULE_SCRIPT = "art-tarantool.lua";
