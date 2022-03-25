e1f27ee3f94fed6e1ff7fd50bd8983b01e249edc
anton.bashirov <anton.sh.local@gmail.com>
2022-03-25 03:34:51 +0300

    ART

--- tarantool/src/test/java/io/art/tarantool/test/TarantoolRouterTest.java
+++ tarantool/src/test/java/io/art/tarantool/test/TarantoolRouterTest.java
@@ -1,5 +1,6 @@
 package io.art.tarantool.test;
 
+import io.art.core.collection.*;
 import io.art.meta.module.*;
 import io.art.meta.test.*;
 import io.art.meta.test.meta.*;
@@ -11,8 +12,14 @@ import io.art.transport.module.*;
 import org.junit.jupiter.api.*;
 import static io.art.core.context.Context.*;
 import static io.art.core.initializer.Initializer.*;
+import static io.art.core.normalizer.ClassIdentifierNormalizer.*;
 import static io.art.core.wrapper.ExceptionWrapper.*;
+import static io.art.meta.test.TestingMetaModelGenerator.generateTestingModel;
+import static io.art.meta.test.meta.MetaMetaTest.MetaIoPackage.MetaArtPackage.MetaMetaPackage.MetaTestPackage.MetaTestingMetaModelClass.*;
 import static io.art.tarantool.Tarantool.*;
+import static io.art.tarantool.model.TarantoolIndexConfiguration.*;
+import static io.art.tarantool.model.TarantoolSpaceConfiguration.*;
+import static io.art.tarantool.module.TarantoolModule.*;
 import static io.art.tarantool.test.constants.TestTarantoolConstants.*;
 import static io.art.tarantool.test.manager.TestTarantoolInstanceManager.*;
 import static io.art.tarantool.test.model.TestStorage.*;
@@ -38,24 +45,24 @@ public class TarantoolRouterTest {
                                 .sharders(TestModelSharders.class))
                 )
         );
-//        tarantoolModule().configuration().getStorageClients()
-//                .get(idByDash(TestStorage.class))
-//                .router()
-//                .call(ROUTER_BOOTSTRAP_FUNCTION)
-//                .block();
-//        tarantool()
-//                .schema(TestStorage.class)
-//                .createSpace(spaceFor(TestingMetaModel.class).ifNotExists(true).build())
-//                .createIndex(indexFor(testingMetaModel(), testModelIndexes().id())
-//                        .configure()
-//                        .ifNotExists(true)
-//                        .unique(true)
-//                        .build())
-//                .createIndex(indexFor(testingMetaModel(), testModelIndexes().f9f16())
-//                        .configure()
-//                        .ifNotExists(true)
-//                        .unique(false)
-//                        .build());
+        tarantoolModule().configuration().getStorageClients()
+                .get(idByDash(TestStorage.class))
+                .router()
+                .call(ROUTER_BOOTSTRAP_FUNCTION)
+                .block();
+        tarantool()
+                .schema(TestStorage.class)
+                .createSpace(spaceFor(TestingMetaModel.class).ifNotExists(true).build())
+                .createIndex(indexFor(testingMetaModel(), testModelIndexes().id())
+                        .configure()
+                        .ifNotExists(true)
+                        .unique(true)
+                        .build())
+                .createIndex(indexFor(testingMetaModel(), testModelIndexes().f9f16())
+                        .configure()
+                        .ifNotExists(true)
+                        .unique(false)
+                        .build());
     }
 
     @AfterAll
@@ -66,23 +73,23 @@ public class TarantoolRouterTest {
 
     @Test
     public void testMutable() {
-//        TestingMetaModel data1 = generateTestingModel().toBuilder().f16("bucket-1").f1(1).build();
-//        TestingMetaModel data2 = generateTestingModel().toBuilder().f16("bucket-2").f1(2).build();
-//
-//        data1.assertEquals(current().shard(testModelSharders().string(), "bucket-1").put(data1));
-//        data2.assertEquals(current().shard(testModelSharders().string(), "bucket-2").put(data2));
-//
-//        ImmutableArray<TestingMetaModel> result1 = current().shard(testModelSharders().string(), "bucket-1").select(1);
-//        ImmutableArray<TestingMetaModel> result2 = current().shard(testModelSharders().string(), "bucket-2").select(2);
-//        ImmutableArray<TestingMetaModel> emptyResult = current().shard(testModelSharders().string(), "bucket-2").select(1);
-//
-//        assertEquals(0, emptyResult.size());
-//
-//        assertEquals(1, result1.size());
-//        data1.assertEquals(result1.get(0));
-//
-//        assertEquals(2, result2.size());
-//        data2.assertEquals(result2.get(0));
+        TestingMetaModel data1 = generateTestingModel().toBuilder().f16("bucket-1").f1(1).build();
+        TestingMetaModel data2 = generateTestingModel().toBuilder().f16("bucket-2").f1(2).build();
+
+        data1.assertEquals(current().shard(testModelSharders().string(), "bucket-1").put(data1));
+        data2.assertEquals(current().shard(testModelSharders().string(), "bucket-2").put(data2));
+
+        ImmutableArray<TestingMetaModel> result1 = current().shard(testModelSharders().string(), "bucket-1").select(1);
+        ImmutableArray<TestingMetaModel> result2 = current().shard(testModelSharders().string(), "bucket-2").select(2);
+        ImmutableArray<TestingMetaModel> emptyResult = current().shard(testModelSharders().string(), "bucket-2").select(1);
+
+        assertEquals(0, emptyResult.size());
+
+        assertEquals(1, result1.size());
+        data1.assertEquals(result1.get(0));
+
+        assertEquals(2, result2.size());
+        data2.assertEquals(result2.get(0));
     }
 
 
