1ef513e477a52535f3b9ffa811db50cae6b1cb44
anton.bashirov <anton.sh.local@gmail.com>
2022-04-02 19:12:09 +0300

    ART

--- tarantool/src/test/java/io/art/tarantool/test/manager/TestTarantoolInstanceManager.java
+++ tarantool/src/test/java/io/art/tarantool/test/manager/TestTarantoolInstanceManager.java
@@ -14,6 +14,7 @@ import static io.art.core.wrapper.ExceptionWrapper.*;
 import static io.art.tarantool.test.constants.TestTarantoolConstants.*;
 import static java.lang.Runtime.*;
 import static java.nio.file.Paths.*;
+import static java.time.Duration.*;
 import static java.util.Objects.*;
 import java.io.*;
 import java.nio.file.*;
@@ -110,5 +111,6 @@ public class TestTarantoolInstanceManager {
 
         wrapExceptionCall(() -> getRuntime().exec(command), TarantoolException::new);
         waitCondition(() -> !TCP.isPortAvailable(port));
+        waitTime(ofSeconds(1));
     }
 }
