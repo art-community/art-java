cf415a4a4ca25b30c99cbf83def7f072519a9955
anton.bashirov <anton.sh.local@gmail.com>
2022-04-02 20:42:14 +0300

    ART

--- tarantool/src/test/resources/test-replica-initializer.lua
+++ tarantool/src/test/resources/test-replica-initializer.lua
@@ -1,4 +1,7 @@
 return function()
+    box.schema.user.create('username', { password = 'password', if_not_exists = true })
+    box.schema.user.grant('username', 'read,write,execute,create,alter,drop', 'universe', nil, { if_not_exists = true })
+
     testChannel = function()
         box.session.push("test")
         box.session.push("test")
