26d3725fa118fde755b231523342e0e442c0e47d
Anton Bashirov <anton.bashirov@rt.ru>
2023-07-12 00:16:38 +0300

    change repo

--- tarantool/src/test/resources/test-shard-1-master.lua
+++ tarantool/src/test/resources/test-shard-1-master.lua
@@ -10,7 +10,7 @@ local cfg = {
     replication_connect_quorum = 0,
     memtx_use_mvcc_engine = true,
     replication_synchro_quorum = 2,
-    sharding = require("test-sharding")ц
+    sharding = require("test-sharding")
 }
 
 local initializer = require("test-shard-initializer")
