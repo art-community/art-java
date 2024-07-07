c2280b6214b314a358d7a1563ef69d2d474a4750
anton <anton.sh.local@gmail.com>
2024-07-07 18:41:24 +0400

    fixes

--- meta/src/main/java/io/art/meta/model/MetaProxy.java
+++ meta/src/main/java/io/art/meta/model/MetaProxy.java
@@ -2,6 +2,8 @@ package io.art.meta.model;
 
 import io.art.core.annotation.*;
 import io.art.core.collection.*;
+
+import static io.art.core.caster.Caster.cast;
 import static io.art.core.factory.MapFactory.*;
 import java.util.*;
 import java.util.function.*;
@@ -9,6 +11,22 @@ import java.util.function.*;
 @Generation
 public abstract class MetaProxy {
     private final Map<MetaMethod<MetaClass<?>, ?>, Function<Object, Object>> invocations;
+
+    protected void run(Function<Object, Object> function) {
+        function.apply(null);
+    }
+
+    protected <T> T get(Function<Object, Object> function) {
+        return cast(function.apply(null));
+    }
+
+    protected <T> T single(Function<Object, Object> function, Object argument) {
+        return cast(function.apply(argument));
+    }
+
+    protected <T> T multiple(Function<Object, Object> function, Object[] arguments) {
+        return cast(function.apply(cast(arguments)));
+    }
 
     public MetaProxy(Map<MetaMethod<MetaClass<?>, ?>, Function<Object, Object>> invocations) {
         this.invocations = invocations;
